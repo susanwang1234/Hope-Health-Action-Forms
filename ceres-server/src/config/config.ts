f6a78da3264bb8349645102f47a823c44caad033
Alden Ilao <ailao@sfu.ca>
2021-09-29 01:54:53 -0700

    12: added environment information and created db module to connect to mysql

--- ceres-server/src/config/config.ts
+++ ceres-server/src/config/config.ts
@@ -10,8 +10,23 @@ const SERVER = {
   port: SERVER_PORT
 };
 
+const DB_PORT = process.env.HOST_NAME || 30061;
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'root';
+const DB_NAME = process.env.DB_NAME || 'HHA';
+const DB_HOST = process.env.DB_HOST || 'localhost';
+
+const DATABASE = {
+  port: DB_PORT,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  name: DB_NAME,
+  host: DB_HOST
+};
+
 const config = {
-  server: SERVER
+  server: SERVER,
+  database: DATABASE
 };
 
 export default config;
