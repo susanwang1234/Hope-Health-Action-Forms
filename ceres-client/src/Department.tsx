52394a9a7959f87117626e47f6dbee60deadb4fc
Alireza Ahmadi <aaa120@sfu.ca>
2021-10-05 22:07:17 -0700

    nav bar is made, routing using BrowserRouter initiated but still in prograss

--- ceres-client/src/Department.tsx
+++ ceres-client/src/Department.tsx
@@ -1,39 +1,52 @@
 import React from "react";
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom' 
+import NavBar from './components/NavBar'
 
 function Department() {
     return(
-        <div className="min-h-screen">
-            <div className="mt-8 grid grid-cols-3 gap-4 h-screen p-6">
-                <div className="text-center mx-6 row-span-3 bg-gray-300 rounded">Case Studies</div>
-                <div className="text-center mx-6 row-span-3 bg-gray-300 rounded">Reports</div>
-                <div className="text-center mx-6 bg-gray-300 rounded">Employee of the Month</div>
-                <div className="text-center mx-6 row-span-2 bg-gray-300 rounded">Leaderboard</div>
-            </div>
-            
-        </div>
+        <Router>
+          <Switch>  
+              <Route exact path='/' component={renderDepartment}/>
+              <NavBar />
+              <Route path='/Department-home-page' component={renderDepartment}/>
+          </Switch>
+        </Router>
+      )
+}
+
+function renderDepartment(){
+    return(
+                <NavBar/>
+                /*{ {CaseStudy()}
+                {dataPreview()}
+                {EmployeeOfTheMonth()}
+                {LeaderBoard()} }*/
     );
 }
 
 function CaseStudy() {
     return(
         <div>
-            <h1>Case Studies</h1>
+            <h1 className="text-center mx-6 row-span-3 bg-gray-300 rounded">Case Studies</h1>
         </div>
     );
 }
 
-function Report() {
+function dataPreview() {
     return(
-        <div>
-            <h1>Reports</h1>
+        <div className="text-center mx-6 row-span-3 bg-gray-300 rounded">
+            <h1>Current statistics</h1>
+            <button>Department Data</button>
+            <button>Edit Data</button>
+            <button>Add New Data</button>g 
         </div>
     );
 }
 
 function EmployeeOfTheMonth() {
     return(
-        <div>
+        <div className="text-center mx-6 bg-gray-300 rounded">
             <h1>Employee of the Month</h1>
         </div>
     );
@@ -41,11 +54,9 @@ function EmployeeOfTheMonth() {
 
 function LeaderBoard() {
     return(
-        <div>
+        <div className="text-center mx-6 row-span-2 bg-gray-300 rounded">
             <h1>Leaderboard</h1>
         </div>
     );
 }
-
-
 export default Department; 
