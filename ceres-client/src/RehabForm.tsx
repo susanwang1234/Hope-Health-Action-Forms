ed1036b24e1a482b2ebb0887a8a2e2fa3aa02723
jyw30 <susan_wang_3@sfu.ca>
2021-10-06 21:46:20 -0700

    added space next to form labels

--- ceres-client/src/RehabForm.tsx
+++ ceres-client/src/RehabForm.tsx
@@ -6,19 +6,19 @@ function RehabForm() {
     return(
         <div className='grey-blocks-form'>
             <h1 className='title-form'>Rehab Monthly Data Collection</h1>
-            <label className='margin-left-50px'>Beds avaliable:</label>
+            <label className='margin-left-50px'>Beds avaliable: </label>
             <input className='margin-bottom-25px' type="number" id="beds-avaliable" name="beds-avaliable" min="0" /><br/>
 
-            <label className='margin-left-50px'>Bed days:</label>
+            <label className='margin-left-50px'>Bed days: </label>
             <input className='margin-bottom-25px' type="number" id="bed-days" name="bed-days" min="0" /><br/>
 
-            <label className='margin-left-50px'>Patient days:</label>
+            <label className='margin-left-50px'>Patient days: </label>
             <input className='margin-bottom-25px' type="number" id="patient-days" name="patient-days" min="0" /><br/>
 
-            <label className='margin-left-50px'>Hospitalized:</label>
+            <label className='margin-left-50px'>Hospitalized: </label>
             <input className='margin-bottom-25px' type="number" id="hospitalized" name="hospitalized" min="0" /><br/>
 
-            <label className='margin-left-50px'>Discharged alive:</label>
+            <label className='margin-left-50px'>Discharged alive: </label>
             <input className='margin-bottom-25px' type="number" id="discharged-alive" name="discharged-alive" min="0" /><br/>
         </div>
     );
