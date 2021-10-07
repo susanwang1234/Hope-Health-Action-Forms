f4715428d637c086d86893732e5e1c51d6ca0fcb
Susan Wang <jyw30@sfu.ca>
2021-10-06 18:36:35 -0700

    added rehab form

---
+++ ceres-client/src/RehabForm.tsx
@@ -0,0 +1,25 @@
+
+import React from "react";
+import './App.css';
+
+function RehabForm() {
+    return(
+        <div>
+            <h1>Rehab Monthly Data Collection</h1>
+            Beds avaliable:
+            <input type="number" id="beds-avaliable" name="beds-avaliable" min="0" /><br/>
+
+            Bed days:
+            <input type="number" id="bed-days" name="bed-days" min="0" /><br/>
+
+            Patient days:
+            <input type="number" id="patient-days" name="patient-days" min="0" /><br/>
+
+            Hospitalized:
+            <input type="number" id="hospitalized" name="hospitalized" min="0" /><br/>
+
+            Discharged alive:
+            <input type="number" id="discharged-alive" name="discharged-alive" min="0" /><br/>
+        </div>
+    );
+}
