463aece1483b45990bbf2c442860f1ab57c3748b
Keegan McGinnis <kmcginni@sfu.ca>
2021-10-06 22:34:42 -0700

    25: Add knexfile configuration file for connecting to db

---
+++ ceres-server/knexfile.ts
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: "mysql2",
+    connection: {
+      host: process.env.MYSQL_HOST || 'ceres-database',
+      port: process.env.MYSQL_PORT || 3306,
+      user: process.env.MYSQL_USER || 'HHA',
+      password: process.env.MYSQL_PASS || 'password',
+      database: process.env.MYSQL_DATABASE || 'ceresdb'
+    }
+  },
+};
