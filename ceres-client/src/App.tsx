75b5bc2e22f819d060d7d584c3269d8868e4554e
Matt Fung <chf2@sfu.ca>
2021-10-06 22:19:20 -0700

    19: Updated scripts to include react-router

--- ceres-client/src/App.tsx
+++ ceres-client/src/App.tsx
@@ -1,25 +1,23 @@
-import React from "react";
+import React from 'react';
 import './App.css';
 import { useForm } from 'react-hook-form';
 import Logo from './images/Logo.png';
 import display from './images/CBR_training_March 21.png';
-import Department from "./Department";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom' 
-import NavBar from './components/NavBar'
-import UserLogin from "./UserLogin";
+import Department from './Department';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import UserLogin from './UserLogin';
 
 function App() {
-  return(
-      <Router>
-        <Switch>  
-            <Route exact path='/' component={UserLogin}/>
-            <Route path='/Department-home-page' component={Department}/>
-            <Route path='bv/Department-data' component={Department}></Route>
-        </Switch>
-      </Router>
-    )
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={UserLogin} />
+        <Route path="/Department-home-page" component={Department} />
+        <Route path="bv/Department-data" component={Department}></Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
-
-
