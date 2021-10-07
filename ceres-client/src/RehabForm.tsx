1f80ad6a493394afd63be18f201cc51dd610ea84
jyw30 <susan_wang_3@sfu.ca>
2021-10-06 21:36:47 -0700

    edited App.css and RehabForm.tsx

--- ceres-client/src/RehabForm.tsx
+++ ceres-client/src/RehabForm.tsx
@@ -5,21 +5,21 @@ import './App.css';
 function RehabForm() {
     return(
         <div className='grey-blocks-form'>
-            <h1 className='header-form'>Rehab Monthly Data Collection</h1>
-            Beds avaliable:
-            <input type="number" id="beds-avaliable" name="beds-avaliable" min="0" /><br/>
+            <h1 className='title-form'>Rehab Monthly Data Collection</h1>
+            <label className='margin-left-50px'>Beds avaliable:</label>
+            <input className='margin-bottom-25px' type="number" id="beds-avaliable" name="beds-avaliable" min="0" /><br/>
 
-            Bed days:
-            <input type="number" id="bed-days" name="bed-days" min="0" /><br/>
+            <label className='margin-left-50px'>Bed days:</label>
+            <input className='margin-bottom-25px' type="number" id="bed-days" name="bed-days" min="0" /><br/>
 
-            Patient days:
-            <input type="number" id="patient-days" name="patient-days" min="0" /><br/>
+            <label className='margin-left-50px'>Patient days:</label>
+            <input className='margin-bottom-25px' type="number" id="patient-days" name="patient-days" min="0" /><br/>
 
-            Hospitalized:
-            <input type="number" id="hospitalized" name="hospitalized" min="0" /><br/>
+            <label className='margin-left-50px'>Hospitalized:</label>
+            <input className='margin-bottom-25px' type="number" id="hospitalized" name="hospitalized" min="0" /><br/>
 
-            Discharged alive:
-            <input type="number" id="discharged-alive" name="discharged-alive" min="0" /><br/>
+            <label className='margin-left-50px'>Discharged alive:</label>
+            <input className='margin-bottom-25px' type="number" id="discharged-alive" name="discharged-alive" min="0" /><br/>
         </div>
     );
 }
