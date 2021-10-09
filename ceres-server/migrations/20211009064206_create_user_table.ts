9eb2272f4b0a813b39cf0ec893dcfd1bd790c4d9
Keegan McGinnis <kmcginni@sfu.ca>
2021-10-09 00:10:10 -0700

    15: Add migration to create user table

---
+++ ceres-server/migrations/20211009064206_create_user_table.ts
@@ -0,0 +1,23 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('User', (table: Knex.CreateTableBuilder) => {
+    table.increments();
+    table.string('username', 50).unique().notNullable();
+    table.string('password', 50).notNullable();
+    table.integer('departmentId')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('Department');
+    table.integer('roleId')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('Role');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('User');
+}
