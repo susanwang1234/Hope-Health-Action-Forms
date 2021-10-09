627283b9533d20c09f99322f0b54de373e70ee56
Keegan McGinnis <kmcginni@sfu.ca>
2021-10-09 16:06:36 -0700

    34: Add migration to create questions table

---
+++ ceres-server/migrations/20211009225937_create_questions_table.ts
@@ -0,0 +1,15 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('Questions', (table: Knex.CreateTableBuilder) => {
+    table.increments();
+    table.string('label').notNullable();
+    table.string('inputType', 20).notNullable();
+    table.string('responseType', 20).notNullable();
+    table.boolean('isMSPP');
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('Questions');
+}
