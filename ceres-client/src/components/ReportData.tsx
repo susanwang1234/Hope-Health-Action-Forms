85cc7e4c4a50f1696991e35d26b725dee26f581c
Alireza Ahmadi <aaa120@sfu.ca>
2021-10-08 23:32:56 -0700

    reportData component now displays data when you click on every element in the list

--- ceres-client/src/components/ReportData.tsx
+++ ceres-client/src/components/ReportData.tsx
@@ -1,34 +1,48 @@
 import React, { ReactNode } from 'react';
 
 class DataReport extends React.Component <any,any>{
-    constructor(props: any){
-        super(props);
-        this.state = {
-            keys: Object.keys(props.data),
-            values: Object.values(props.data),
-            pairs: [],
+    render(){
+        if(this.props.data === null){
+            return(<p>nothing</p>);
+        }else{
+            let keys = Object.keys(this.props.data);
+            let values: string[] = Object.values(this.props.data);
+            let pairs: string[][] = [];
+            for(let i = 0; i < keys.length; i++){
+                let pair:string[] = [keys[i],values[i]];
+                pairs.push(pair);
+            }
+            return(
+                <table>
+                    {pairs.map((pair: string[]) =>
+                    <tr>
+                        {pair.map((item: string) =>
+                        <td>
+                            {item}
+                        </td>
+                        )}
+                    </tr>
+                    )}
+                </table>
+            );
         }
+    }
+    updateData(){
+        this.setState({
+                keys: Object.keys(this.props.data),
+                values: Object.values(this.props.data),
+                pairs: [],
+            })
+            console.log(this.state.keys.length)
+            console.log("dfasdsa");
         for(let i = 0; i < this.state.keys.length; i++){
             let pair = [this.state.keys[i],this.state.values[i]]
             this.state.pairs.push(pair);
-            console.log(this.state.keys)
         }
-    };
-    render(){
-        return(
-            <table>
-                {this.state.pairs.map((pair: string[]) =>
-                 <tr>
-                     {pair.map((item: string) =>
-                      <td>
-                          {item}
-                      </td>
-                      )}
-                </tr>
-                )}
-            </table>
-        )
     }
+    // componentWillReceiveProps(nextProps : any) {
+    //     this.setState({ data: nextProps.data });  
+    // }
     
 }
 export default DataReport;
--- ceres-client/src/dataPage.tsx
+++ ceres-client/src/dataPage.tsx
@@ -3,21 +3,26 @@ import './App.css';
 import ReportElement from './components/ReportElement';
 import ReportData from './components/ReportData'
 
-let dummyDataObjects = [{date: "sdasdas", employee: "fdfdsdf", randomss: "datar"},
-                        {date: "gggggg", employee: "bitch", randomss:"adsdas"}];
-
-function dataPage(){
-    return(
-        <div>
-            <ul className="reportsList">
-                {dummyDataObjects.map((report,index) =><ReportElement data={report} 
-                onClick ={() => handleClick(index)}/>)}
-            </ul>
-            <ReportData data={dummyDataObjects[1]}/>
-        </div>
-    )
-}
-function handleClick(index: number) {
-    console.log("hthis call is from daddy");
+class dataPage extends React.Component <any[], any>{
+    constructor(props: any[]){
+        super(props);
+        this.state = {reports: [{date: "sdasdas", employee: "fdfdsdf", randomss: "datar"},
+                     {date: "gggggg", employee: "bitch", randomss:"adsdas"}], displayingData: null
+                    };
+    }
+    render(){
+        return(
+            <div>
+                <ul className="reportsList">
+                    {this.state.reports.map((report: any,index: number) =><ReportElement data={report} 
+                    onClick ={() => this.handleClick(index)}/>)}
+                </ul>
+                <ReportData data={this.state.displayingData}/>
+            </div>
+        );
+    }
+    handleClick(index: number) : void{
+        this.setState({displayingData: this.state.reports[index]});
+    }
 }
 export default dataPage;
