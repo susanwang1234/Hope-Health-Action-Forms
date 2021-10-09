eedcbe78704582b667d974e20493c23fcf64c332
Keegan McGinnis <kmcginni@sfu.ca>
2021-10-09 16:14:05 -0700

    34: Add migration to create department questions table

---
+++ ceres-server/migrations/20211009230819_create_department_questions_table.ts
@@ -0,0 +1,16 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('DepartmentQuestions', (table: Knex.CreateTableBuilder) => {
+    table.increments();
+    table.integer('departmentId').unsigned().notNullable().references('id').inTable('Department');
+    table.integer('questionId').unsigned().notNullable().references('id').inTable('Questions');
+    table.boolean('isRequired');
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('DepartmentQuestions');
+}
+
