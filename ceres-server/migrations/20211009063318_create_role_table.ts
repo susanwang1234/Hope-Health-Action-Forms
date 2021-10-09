2195ae8c4bcd541fa565be80a47aa61681f214f2
Keegan McGinnis <kmcginni@sfu.ca>
2021-10-08 23:40:40 -0700

    15: Add migration to create role table, added typing to migrations

--- ceres-server/migrations/20211009063318_create_role_table.ts
+++ ceres-server/migrations/20211009063318_create_role_table.ts
@@ -1,12 +1,12 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('Department', (table: Knex.CreateTableBuilder) => {
+  return knex.schema.createTable('Role', (table: Knex.CreateTableBuilder) => {
     table.increments();
     table.string('name', 50).notNullable();
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('Department');
+  return knex.schema.dropTable('Role');
 }
