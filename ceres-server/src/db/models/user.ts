d634ab2cc5e7a0128b44a254bc046dae9745285e
Alden Ilao <ailao@sfu.ca>
2021-10-09 01:55:46 -0700

    15: created seed to populate table and migrated to knex
    additionally fixed bugs from sql query

--- ceres-server/src/db/models/user.ts
+++ ceres-server/src/db/models/user.ts
@@ -3,17 +3,15 @@ import { db } from '../mysql';
 import { OkPacket, RowDataPacket } from 'mysql2';
 
 export const findOne = (username: string, callback: Function) => {
-  const queryString = `SELECT U.id, U.username, U.pwd, D.departmentName, R.roleName 
+  const queryString = `SELECT U.id as id, U.username as username, U.password as password, D.name as departmentName, R.name as roleName
                         FROM User AS U, Role AS R, Department AS D
-                        WHERE (U.username = ? AND R.id = U.roleID AND D.id = U.departmentID)`;
+                        WHERE (U.username = ? AND R.id = U.roleId AND D.id = U.departmentId)`;
 
   db.query(queryString, username, (err, result) => {
     if (err) {
       callback(err);
     }
-
     const row = (<RowDataPacket>result)[0];
-
     if (row === undefined) {
       callback(null, undefined);
       return;
@@ -22,11 +20,11 @@ export const findOne = (username: string, callback: Function) => {
     const user: User = {
       id: row.id,
       username: row.username,
-      pwd: row.pwd,
+      pwd: row.password,
       departmentName: row.departmentName,
       roleName: row.roleName
     };
-
+    console.log(user);
     callback(null, user);
   });
 };
