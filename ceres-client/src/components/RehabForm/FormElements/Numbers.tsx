ad3b5699c9aafabefddf8b9cae4b83cd14838be5
Matt Fung <chf2@sfu.ca>
2021-10-10 16:04:39 -0700

    33: Run prettier on files changed

--- ceres-client/src/components/RehabForm/FormElements/Numbers.tsx
+++ ceres-client/src/components/RehabForm/FormElements/Numbers.tsx
@@ -1,17 +1,21 @@
 import React, { useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Form, Row, Col, Nav } from 'react-bootstrap';
-import '../RehabForm.css'
-import { FormContext } from "../FormContext";
+import '../RehabForm.css';
+import { FormContext } from '../FormContext';
 
 // Citation: https://medium.com/swlh/how-to-generate-dynamic-form-from-json-with-react-5d70386bb38b
 
-const Numbers = ({ my_field_id, my_field_label, my_field_placeholder, my_field_value, my_field_mandatory } : any) => {
-  const {handleChange} : any = useContext(FormContext)
+const Numbers = ({ my_field_id, my_field_label, my_field_placeholder, my_field_value, my_field_mandatory }: any) => {
+  const { handleChange }: any = useContext(FormContext);
 
-  let heading : any = {my_field_label}
-  if(my_field_mandatory == true) {
-     heading = (<>{my_field_label} <div className='required'>(Required)</div></>)
+  let heading: any = { my_field_label };
+  if (my_field_mandatory === true) {
+    heading = (
+      <>
+        {my_field_label} <div className="required">(Required)</div>
+      </>
+    );
   }
 
   return (
@@ -21,8 +25,17 @@ const Numbers = ({ my_field_id, my_field_label, my_field_placeholder, my_field_v
           {heading}
         </Form.Label>
         <Col>
-          <Form.Control className="width-20-percent" size="sm" type="number" id="number-entry" name="number-entry" min="0" placeholder={my_field_placeholder?my_field_placeholder:''} 
-          value={my_field_value}  onChange={event=>handleChange(my_field_id,event)}/>
+          <Form.Control
+            className="width-20-percent"
+            size="sm"
+            type="number"
+            id="number-entry"
+            name="number-entry"
+            min="0"
+            placeholder={my_field_placeholder ? my_field_placeholder : ''}
+            value={my_field_value}
+            onInput={(event: any) => handleChange(my_field_id, event)}
+          />
         </Col>
       </Row>
       <br />
