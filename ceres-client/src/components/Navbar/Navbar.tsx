2c50386d464b8af0aa08cf42c0ac546f54092eda
ysaleem <ysaleem@sfu.ca>
2021-10-10 16:17:25 -0700

    Added Navbar to all pages (except login) Navbar changes active link based on page you're on

--- ceres-client/src/components/Navbar/Navbar.tsx
+++ ceres-client/src/components/Navbar/Navbar.tsx
@@ -1,32 +1,98 @@
 /*Citation: https://github.com/mustafaerden/react-admin-dashboard*/
 import './Navbar.css';
 import logo from '../../images/navlogo.png';
+import { useLocation } from 'react-router-dom'
+
+
+
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="nav_icon">
-        <i className="fa fa-bars" aria-hidden="true"></i>
-      </div>
-      <div className="navbar__left">
-        <div className="navlogo">
-          <img src={logo} alt="HHA Logo" width="200px" />
+
+  let pagePath = window.location.pathname;
+  console.log(pagePath);
+
+  if (pagePath == "/dashboard") {
+    return (
+
+      <nav className="navbar">
+        <div className="nav_icon">
+          <i className="fa fa-bars" aria-hidden="true"></i>
         </div>
-        <a className="active_link" href="/dashboard">
-          Home
-        </a>
-        <a href="/data-page">View Data</a>
-        <a href="/submit-report">Submit Data</a>
-      </div>
-      <div className="navbar__right">
-        <a href="/">
-          <i className="fa fa-search" aria-hidden="true">
-            Logout
-          </i>
-        </a>
-      </div>
-    </nav>
-  );
+        <div className="navbar__left">
+          <div className="navlogo">
+            <img src={logo} alt="HHA Logo" width="200px" />
+          </div>
+          <a className="active_link" href="/dashboard">
+            Home
+          </a>
+          <a href="/data-page">View Data</a>
+          <a href="/submit-report">Submit Data</a>
+        </div>
+        <div className="navbar__right">
+          <a href="/">
+            <i className="fa fa-search" aria-hidden="true">
+              Logout
+            </i>
+          </a>
+        </div>
+      </nav>
+    );
+  } else if (pagePath == "/data-page") {
+    return (
+
+      <nav className="navbar">
+        <div className="nav_icon">
+          <i className="fa fa-bars" aria-hidden="true"></i>
+        </div>
+        <div className="navbar__left">
+          <div className="navlogo">
+            <img src={logo} alt="HHA Logo" width="200px" />
+          </div>
+          <a href="/dashboard">
+            Home
+          </a>
+          <a className="active_link" href="/data-page">View Data</a>
+          <a href="/submit-report">Submit Data</a>
+        </div>
+        <div className="navbar__right">
+          <a href="/">
+            <i className="fa fa-search" aria-hidden="true">
+              Logout
+            </i>
+          </a>
+        </div>
+      </nav>
+    );
+  } else if (pagePath == "/submit-report") {
+    return (
+
+      <nav className="navbar">
+        <div className="nav_icon">
+          <i className="fa fa-bars" aria-hidden="true"></i>
+        </div>
+        <div className="navbar__left">
+          <div className="navlogo">
+            <img src={logo} alt="HHA Logo" width="200px" />
+          </div>
+          <a href="/dashboard">
+            Home
+          </a>
+          <a href="/data-page">View Data</a>
+          <a className="active_link" href="/submit-report">Submit Data</a>
+        </div>
+        <div className="navbar__right">
+          <a href="/">
+            <i className="fa fa-search" aria-hidden="true">
+              Logout
+            </i>
+          </a>
+        </div>
+      </nav>
+    );
+  } else {
+    return(<div/>);
+  }
+  
 };
 
 export default Navbar;
