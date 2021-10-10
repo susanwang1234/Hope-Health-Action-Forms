48fcaad53ea6630483ea9a554cf75393e1b528de
Aida Kadiric <akadiric@sfu.ca>
2021-10-10 02:21:23 -0700

    Reformat using Prettier

--- ceres-client/src/components/CaseStudy/CaseStudy.tsx
+++ ceres-client/src/components/CaseStudy/CaseStudy.tsx
@@ -2,18 +2,15 @@ import './CaseStudy.css';
 import Navbar from '../Navbar/Navbar';
 import { useHistory } from 'react-router-dom';
 
-
 const CaseStudy = () => {
-
-    let history = useHistory();
-    const onClick = () => {
-
-    }
-    return(
-        <main>
-            <Navbar/>
-        </main>
-    )
-}
+  let history = useHistory();
+  const onClick = () => {};
+  return (
+    <main>
+      <Navbar />
+      <div className="background">Case Study</div>
+    </main>
+  );
+};
 
 export default CaseStudy;
