bec626ecdf6c701f70d6ec4ff54b2375171ec43b
jyw30 <susan_wang_3@sfu.ca>
2021-10-10 06:35:50 -0700

    comment clean up

--- ceres-client/src/components/RehabForm/RehabForm.tsx
+++ ceres-client/src/components/RehabForm/RehabForm.tsx
@@ -9,10 +9,9 @@ import Element from './Elements';
 
 console.log('JSONfile', JSONfile)
 
-// Citation: https://medium.com/swlh/how-to-generate-dynamic-form-from-json-with-react-5d70386bb38b
-//class RehabForm extends Component {
 function RehabForm() {
   
+  // Citation: https://medium.com/swlh/how-to-generate-dynamic-form-from-json-with-react-5d70386bb38b
   const [elements, setElements] = useState<any | null>(null);
   useEffect(() => { 
     return setElements(JSONfile[0]);
@@ -42,77 +41,6 @@ function RehabForm() {
         </div>
     </main>
     );
-  }
-
-  /*
-  render() {
-    return (
-      <main>
-        <Navbar/>
-        <div>
-          <div className="grey-blocks-form">
-            <Form.Label column="lg" >Rehab Monthly Data Collection</Form.Label>
-            <Row>
-              <Form.Label className="margin-left-50px" column lg={2}>
-                Beds avaliable:
-              </Form.Label>
-              <Col>
-                <Form.Control className="width-20-percent" size="sm" type="number" id="beds-avaliable" name="beds-avaliable" min="0" />
-              </Col>
-            </Row>
-            <br />
-
-            <Row>
-              <Form.Label className="margin-left-50px" column lg={2}>
-                Bed days:
-              </Form.Label>
-              <Col>
-                <Form.Control className="width-20-percent" size="sm" type="number" id="bed-days" name="bed-days" min="0" />
-              </Col>
-            </Row>
-            <br />
-
-            <Row>
-              <Form.Label className="margin-left-50px" column lg={2}>
-                Patient days:
-              </Form.Label>
-              <Col>
-                <Form.Control className="width-20-percent" size="sm" type="number" id="patient-days" name="patient-days" min="0" />
-              </Col>
-            </Row>
-            <br />
-
-            <Row>
-              <Form.Label className="margin-left-50px" column lg={2}>
-                Hospitalized:
-              </Form.Label>
-              <Col>
-                <Form.Control className="width-20-percent" size="sm" type="number" id="hospitalized" name="hospitalized" min="0" />
-              </Col>
-            </Row>
-            <br />
-
-            <Row>
-              <Form.Label className="margin-left-50px" column lg={2}>
-                Discharged alive:
-              </Form.Label>
-              <Col>
-                <Form.Control className="width-20-percent" size="sm" type="number" id="discharged-alive" name="discharged-alive" min="0" />
-              </Col>
-            </Row>
-            <br />
-          </div>
-
-          <div className="button-form">
-            <Button variant="primary" href="/dashboard" onClick={this.changeSaved}>
-              Save
-            </Button>{' '}
-          </div>
-        </div>
-      </main>
-    );
-  }
-}
-*/
+  }    
 
 export default RehabForm;
