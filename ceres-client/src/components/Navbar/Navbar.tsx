41f38f25f00c5254c92425c65a958d43cbaf9536
ysaleem <ysaleem@sfu.ca>
2021-10-10 23:16:03 -0700

    Refactored Navbar code to reduce code repetition

--- ceres-client/src/components/Navbar/Navbar.tsx
+++ ceres-client/src/components/Navbar/Navbar.tsx
@@ -3,95 +3,46 @@ import './Navbar.css';
 import logo from '../../images/navlogo.png';
 import { useLocation } from 'react-router-dom'
 
-
-
-
 const Navbar = () => {
 
   let pagePath = window.location.pathname;
   console.log(pagePath);
 
+  let homeClass = "";
+  let dataClass = "";
+  let submitClass = "";
+
   if (pagePath == "/dashboard") {
-    return (
-
-      <nav className="navbar">
-        <div className="nav_icon">
-          <i className="fa fa-bars" aria-hidden="true"></i>
-        </div>
-        <div className="navbar__left">
-          <div className="navlogo">
-            <img src={logo} alt="HHA Logo" width="200px" />
-          </div>
-          <a className="active_link" href="/dashboard">
-            Home
-          </a>
-          <a href="/data-page">View Data</a>
-          <a href="/submit-report">Submit Data</a>
-        </div>
-        <div className="navbar__right">
-          <a href="/">
-            <i className="fa fa-search" aria-hidden="true">
-              Logout
-            </i>
-          </a>
-        </div>
-      </nav>
-    );
+    homeClass = "active_link";
   } else if (pagePath == "/data-page") {
-    return (
-
-      <nav className="navbar">
-        <div className="nav_icon">
-          <i className="fa fa-bars" aria-hidden="true"></i>
-        </div>
-        <div className="navbar__left">
-          <div className="navlogo">
-            <img src={logo} alt="HHA Logo" width="200px" />
-          </div>
-          <a href="/dashboard">
-            Home
-          </a>
-          <a className="active_link" href="/data-page">View Data</a>
-          <a href="/submit-report">Submit Data</a>
-        </div>
-        <div className="navbar__right">
-          <a href="/">
-            <i className="fa fa-search" aria-hidden="true">
-              Logout
-            </i>
-          </a>
-        </div>
-      </nav>
-    );
+    dataClass = "active_link";
   } else if (pagePath == "/submit-report") {
-    return (
-
-      <nav className="navbar">
-        <div className="nav_icon">
-          <i className="fa fa-bars" aria-hidden="true"></i>
-        </div>
-        <div className="navbar__left">
-          <div className="navlogo">
-            <img src={logo} alt="HHA Logo" width="200px" />
-          </div>
-          <a href="/dashboard">
-            Home
-          </a>
-          <a href="/data-page">View Data</a>
-          <a className="active_link" href="/submit-report">Submit Data</a>
-        </div>
-        <div className="navbar__right">
-          <a href="/">
-            <i className="fa fa-search" aria-hidden="true">
-              Logout
-            </i>
-          </a>
-        </div>
-      </nav>
-    );
-  } else {
-    return(<div/>);
+    submitClass = "active_link";
   }
+
+  return (
+
+    <nav className="navbar">
+      <div className="nav_icon">
+        <i className="fa fa-bars" aria-hidden="true"></i>
+      </div>
+      <div className="navbar__left">
+        <div className="navlogo">
+          <img src={logo} alt="HHA Logo" width="200px" />
+        </div>
+        <a className={homeClass} href="/dashboard">Home</a>
+        <a className = {dataClass} href="/data-page">View Data</a>
+        <a className = {submitClass} href="/submit-report">Submit Data</a>
+      </div>
+      <div className="navbar__right">
+        <a href="/">
+          <i className="fa fa-search" aria-hidden="true">
+            Logout
+          </i>
+        </a>
+      </div>
+    </nav>
+  );
   
 };
 
