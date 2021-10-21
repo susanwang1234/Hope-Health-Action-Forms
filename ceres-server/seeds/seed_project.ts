2e7dde64c6d3d77364415f65b4af82f1657fefb7
Keegan McGinnis <kmcginni@sfu.ca>
2021-10-20 23:17:36 -0700

    39: Add NICUPaeds department with MSPP data via global seed file

--- ceres-server/seeds/seed_project.ts
+++ ceres-server/seeds/seed_project.ts
@@ -1,7 +1,7 @@
 import { Knex } from 'knex';
 
 export async function seed(knex: Knex): Promise<void> {
-  // Delete All existing entries, keeping in mind of foreign key constraints 
+  // Delete All existing entries, keeping in mind of foreign key constraints
   await knex('Dummies').del();
   await knex('Rehab_Report').del();
   await knex('User').del();
@@ -24,23 +24,39 @@ export async function seed(knex: Knex): Promise<void> {
     { id: 10, label: 'Transfers', inputType: 'text', responseType: 'number', isMSPP: true },
     { id: 11, label: 'Self-discharged', inputType: 'text', responseType: 'number', isMSPP: true },
     { id: 12, label: 'Stayed in the ward', inputType: 'text', responseType: 'number', isMSPP: true },
-    { id: 13, label: 'Admissions', inputType: 'text', responseType: 'number', isMSPP: true },
+    { id: 13, label: 'Admissions', inputType: 'text', responseType: 'number', isMSPP: true }
   ]);
-  await knex('Department').insert([{ id: 1, name: 'Rehab' }]);
+  await knex('Department').insert([
+    { id: 1, name: 'Rehab' },
+    { id: 2, name: 'NICUPaeds' }
+  ]);
   await knex('DepartmentQuestion').insert([
-    { id: 1, departmentId: 1, questionId: 1, isRequired: true},
-    { id: 2, departmentId: 1, questionId: 2, isRequired: true},
-    { id: 3, departmentId: 1, questionId: 3, isRequired: true},
-    { id: 4, departmentId: 1, questionId: 4, isRequired: true},
-    { id: 5, departmentId: 1, questionId: 5, isRequired: true},
-    { id: 6, departmentId: 1, questionId: 6, isRequired: true},
-    { id: 7, departmentId: 1, questionId: 7, isRequired: true},
-    { id: 8, departmentId: 1, questionId: 8, isRequired: true},
-    { id: 9, departmentId: 1, questionId: 9, isRequired: true},
-    { id: 10, departmentId: 1, questionId: 10, isRequired: true},
-    { id: 11, departmentId: 1, questionId: 11, isRequired: true},
-    { id: 12, departmentId: 1, questionId: 12, isRequired: true},
-    { id: 13, departmentId: 1, questionId: 13, isRequired: true},
+    { id: 1, departmentId: 1, questionId: 1, isRequired: true },
+    { id: 2, departmentId: 1, questionId: 2, isRequired: true },
+    { id: 3, departmentId: 1, questionId: 3, isRequired: true },
+    { id: 4, departmentId: 1, questionId: 4, isRequired: true },
+    { id: 5, departmentId: 1, questionId: 5, isRequired: true },
+    { id: 6, departmentId: 1, questionId: 6, isRequired: true },
+    { id: 7, departmentId: 1, questionId: 7, isRequired: true },
+    { id: 8, departmentId: 1, questionId: 8, isRequired: true },
+    { id: 9, departmentId: 1, questionId: 9, isRequired: true },
+    { id: 10, departmentId: 1, questionId: 10, isRequired: true },
+    { id: 11, departmentId: 1, questionId: 11, isRequired: true },
+    { id: 12, departmentId: 1, questionId: 12, isRequired: true },
+    { id: 13, departmentId: 1, questionId: 13, isRequired: true },
+    { id: 14, departmentId: 2, questionId: 1, isRequired: true },
+    { id: 15, departmentId: 2, questionId: 2, isRequired: true },
+    { id: 16, departmentId: 2, questionId: 3, isRequired: true },
+    { id: 17, departmentId: 2, questionId: 4, isRequired: true },
+    { id: 18, departmentId: 2, questionId: 5, isRequired: true },
+    { id: 19, departmentId: 2, questionId: 6, isRequired: true },
+    { id: 20, departmentId: 2, questionId: 7, isRequired: true },
+    { id: 21, departmentId: 2, questionId: 8, isRequired: true },
+    { id: 22, departmentId: 2, questionId: 9, isRequired: true },
+    { id: 23, departmentId: 2, questionId: 10, isRequired: true },
+    { id: 24, departmentId: 2, questionId: 11, isRequired: true },
+    { id: 25, departmentId: 2, questionId: 12, isRequired: true },
+    { id: 26, departmentId: 2, questionId: 13, isRequired: true }
   ]);
   await knex('Role').insert([
     { id: 1, name: 'admin' },
