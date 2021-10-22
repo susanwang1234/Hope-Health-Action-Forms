f19d08ab6545454cd8a5645d39563d99a219ba48
Keegan McGinnis <kmcginni@sfu.ca>
2021-10-21 23:55:54 -0700

    39: Create insert functions for inserting default data to DB, and refactor global seed file to use the insert functions

--- ceres-server/seeds/seed_project.ts
+++ ceres-server/seeds/seed_project.ts
@@ -2,6 +2,11 @@ import { Knex } from 'knex';
 import { insertQuestions } from './inserts/insert_questions';
 import { insertRehabDepartmentQuestions } from './inserts/insert_rehab_department_questions';
 import { insertNICUPaedsDepartmentQuestions } from './inserts/insert_nicupaeds_department_questions';
+import { insertDepartments } from './inserts/insert_departments';
+import { insertUsers } from './inserts/insert_users';
+import { insertRoles } from './inserts/insert_roles';
+import { insertRehabReports } from './inserts/insert_rehab_reports';
+import { insertDummies } from './inserts/insert_dummies';
 
 export async function seed(knex: Knex): Promise<void> {
   // Delete All existing entries, keeping in mind of foreign key constraints
@@ -15,58 +20,11 @@ export async function seed(knex: Knex): Promise<void> {
 
   // Insert seed entries
   await insertQuestions(knex);
-  await knex('Department').insert([
-    { id: 1, name: 'Rehab' },
-    { id: 2, name: 'NICUPaeds' }
-  ]);
+  await insertDepartments(knex);
   await insertRehabDepartmentQuestions(knex);
   await insertNICUPaedsDepartmentQuestions(knex);
-  await knex('Role').insert([
-    { id: 1, name: 'admin' },
-    { id: 2, name: 'user' },
-    { id: 3, name: 'departmentHead' }
-  ]);
-  await knex('User').insert([{ id: 1, username: 'admin', password: '$2b$12$kUy4kEGLkdmB9hgSxtyOYetqixdHXOWOa/OSNKcYopCZVhQogwjOm', departmentId: 1, roleId: 1 }]);
-  await knex('Rehab_Report').insert([
-    {
-      id: 1,
-      curr_date: '2021-10-4 20:53:15',
-      beds_available: 19,
-      bed_days: 434,
-      patient_days: 377,
-      hospitalised: 17,
-      discharged: 2,
-      self_discharges: 1,
-      deaths_before_48: 1,
-      deaths_after_48: 0,
-      days_hospitalised: 334,
-      referrals: 0,
-      transfers: 0,
-      stays: 13,
-      admissions: 4,
-      outpatients: 16
-    },
-    {
-      id: 2,
-      curr_date: '2021-10-5 07:44:04',
-      beds_available: 22,
-      bed_days: 435,
-      patient_days: 378,
-      hospitalised: 17,
-      discharged: 2,
-      self_discharges: 1,
-      deaths_before_48: 1,
-      deaths_after_48: 0,
-      days_hospitalised: 335,
-      referrals: 0,
-      transfers: 0,
-      stays: 13,
-      admissions: 4,
-      outpatients: 16
-    }
-  ]);
-  await knex('Dummies').insert([
-    { id: 1, dummies_name: 'Dummy1', dummies_info: 111 },
-    { id: 2, dummies_name: 'Dummy2', dummies_info: 222 }
-  ]);
+  await insertRoles(knex);
+  await insertUsers(knex);
+  await insertRehabReports(knex);
+  await insertDummies(knex);
 }
