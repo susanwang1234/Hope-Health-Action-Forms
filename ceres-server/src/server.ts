fdc790d48e38168a7e58e00d3f7e6d05cce41d3a
Keegan McGinnis <kmcginni@sfu.ca>
2021-10-24 00:14:20 -0700

    39: Add department form route to new structure

--- ceres-server/src/server.ts
+++ ceres-server/src/server.ts
@@ -8,6 +8,7 @@ import dashboardRoutes from './routes/dashboard';
 import departmentRoutes from './routes/department';
 import dummyRoutes from './routes/dummy';
 import rehabReportRoutes from './routes/rehabReport';
+import departmentFormRoutes from './routes/departmentForm';
 import routes from './routes/index';
 import passport from 'passport';
 import './middlewares/passport-strategies.mw.ts';
@@ -77,6 +78,7 @@ export function linkRoutes(router: Application) {
   router.use('/department', departmentRoutes);
   router.use('/dummy', dummyRoutes);
   router.use('/rehab_report', rehabReportRoutes);
+  router.use('/department_form', departmentFormRoutes);
 }
 
 export function enableErrorHandling(router: Application) {
