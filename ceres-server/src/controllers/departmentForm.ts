229f3ef776323b4b0f1086c8a60c26c1c6265a85
Keegan McGinnis <kmcginni@sfu.ca>
2021-10-24 10:55:24 -0700

    39: Add 404 error when department doesn't exist or has not questions, fix query to return correct id

--- ceres-server/src/controllers/departmentForm.ts
+++ ceres-server/src/controllers/departmentForm.ts
@@ -11,14 +11,18 @@ const getDepartmentFormById = async (req: Request, res: Response, next: NextFunc
     res.status(400).send({ error: 'Incorrect usage for /departmentForm/:id , id must be a positive integer' });
     return;
   }
-  // assert department exists, else send 404
+
   try {
-    const questions = await Knex.select('*')
+    const questions = await Knex.select('Department.*', 'Question.*', 'DepartmentQuestion.*')
       .from('DepartmentQuestion')
       .join('Question', 'DepartmentQuestion.questionId', '=', 'Question.id')
       .join('Department', 'DepartmentQuestion.departmentId', '=', 'Department.id')
       .where('Department.id', departmentId);
     logging.info(NAMESPACE, `FETCHED DEPARTMENT FORM FOR DEPARTMENT ${departmentId}`, questions);
+    if (!questions.length) {
+      res.status(404).send({ error: 'Department does not exist or its form structure is empty' });
+      return;
+    }
     res.send(questions);
   } catch (error: any) {
     logging.error(NAMESPACE, error.message, error);
