621d68cc2d502fc6e0847d618f81d09a814413e2
Matt Fung <chf2@sfu.ca>
2021-10-25 03:13:25 -0700

    22: Refactor tests for creating server

--- ceres-server/src/test/testCreateServer.spec.ts
+++ ceres-server/src/test/testCreateServer.spec.ts
@@ -1,31 +1,30 @@
 import request from 'supertest';
 import http from 'http';
-import { createServer, linkRoutes, sendFirstRequest } from '../server';
+import { createServer, sendFirstRequest } from '../server';
 
-// Test 1: Create a working server
-const test1 = createServer();
-sendFirstRequest(test1);
-linkRoutes(test1);
-const httpServerTest1 = http.createServer(test1);
-const port: number = parseInt(<string>process.env.PORT, 10) || 4000;
-httpServerTest1.listen(port, () => {
-  console.log('server running on port ${port}');
-});
-
-// Test 2: Create a non-working server
-const test2 = createServer();
-
-describe('testCreateServerSuccess', function () {
-  it('Server is successfully instantiated', function (done) {
-    request(test1).get('/').expect(200, done);
-  });
-});
-
-describe('testCreateServerFailure', function () {
+// Test 1: Create a non-working server
+describe('testCreateServerFailure', () => {
+  const test1 = createServer();
   it('Server is unsuccessfully instantiated', function (done) {
-    request(test2).get('/').expect(404, done);
+    request(test1).get('/').expect(404, done);
   });
 });
 
-// Close running servers
-httpServerTest1.close();
+// Test 2: Create a working server
+describe('testCreateServerSuccess', () => {
+  const test2 = createServer();
+  sendFirstRequest(test2);
+  const httpServertest2 = http.createServer(test2);
+  const port: number = parseInt(<string>process.env.PORT, 10) || 4000;
+  before('Create a working server', () => {
+    httpServertest2.listen(port, () => {
+      console.log('server running on port ${port}');
+    });
+  });
+  after('Close a working server', () => {
+    httpServertest2.close();
+  });
+  it('Server is successfully instantiated', (done) => {
+    request(httpServertest2).get('/').expect(200, done);
+  });
+});
