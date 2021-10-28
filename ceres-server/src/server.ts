8f2e4a183f07f660670000d4f3862af42e9db896
Matt Fung <chf2@sfu.ca>
2021-10-27 18:58:19 -0700

    67: Add GET request for roles

--- ceres-server/src/server.ts
+++ ceres-server/src/server.ts
@@ -9,6 +9,7 @@ import departmentRoutes from './routes/departmentRoute';
 import dummyRoutes from './routes/dummyRoute';
 import rehabReportRoutes from './routes/rehabReportRoute';
 import departmentFormRoutes from './routes/departmentFormRoute';
+import roleRoutes from './routes/roleRoute';
 import routes from './routes/indexRoute';
 import passport from 'passport';
 import './middlewares/passport-strategies.mw.ts';
@@ -79,6 +80,7 @@ export function enableRoutes(router: Application) {
   router.use('/dummy', dummyRoutes);
   router.use('/rehab_report', rehabReportRoutes);
   router.use('/department_form', departmentFormRoutes);
+  router.use('/role', roleRoutes);
 }
 
 export function enableErrorHandling(router: Application) {
