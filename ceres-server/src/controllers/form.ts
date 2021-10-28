5750b4d211779a0296db338a1da3b02d7ccb9de4
Keegan McGinnis <kmcginni@sfu.ca>
2021-10-27 23:37:14 -0700

    40: Add logic to create new form in db

--- ceres-server/src/controllers/form.ts
+++ ceres-server/src/controllers/form.ts
@@ -8,9 +8,18 @@ const createNewForm = async (req: Request, res: Response, next: NextFunction) =>
   logging.info(NAMESPACE, 'Creating new form');
   const departmentId: number = +req.body.department_id;
   if (!departmentId || departmentId < 0) {
-    res.status(400).send({ error: 'Incorrect usage for /form/:department_id , department_id must be a positive integer' });
+    res.status(400).send({ error: 'Incorrect usage for POST /form, department_id must be a positive integer' });
   }
-  res.send({ id: departmentId });
+
+  try {
+    const newFormId = await Knex('Form').insert({ departmentId: departmentId });
+    const newForm = await Knex.select('*').from('Form').where('id', newFormId);
+    logging.info(NAMESPACE, `Created new form for department ${departmentId}`, newForm);
+    res.status(201).send(newForm);
+  } catch (error: any) {
+    logging.error(NAMESPACE, error.message, error);
+    res.status(500).send(error);
+  }
 };
 
 export default { createNewForm };
