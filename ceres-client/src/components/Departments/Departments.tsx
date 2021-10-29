7aa63929058363181582fb53d67376dc8a5f2d68
Aida Kadiric <akadiric@sfu.ca>
2021-10-29 03:17:02 -0700

    Add check icon

--- ceres-client/src/components/Departments/Departments.tsx
+++ ceres-client/src/components/Departments/Departments.tsx
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 import DepartmentData from "./Departments.json";
+import ToDoData from "./ToDo.json";
 import './Departments.css';
-// <button type="submit" className="view_button">View Department</button>
+//source:https://css.gg/check-o
+/*
+                                     if (DepartmentDetail.name == "Maternity") {
+                                                <div className="checkmark_icon">
+                                                </div>
+                                            }
+*/
 
 class Departments extends Component {
     render() {
@@ -12,6 +19,14 @@ class Departments extends Component {
                             return <div className="card">
                                         <div className="card_inner">
                                             <h2>{DepartmentDetail.name}</h2>
+                                                <div className="checkmark_icon">
+                                                    <div className="checkmark">
+                                                        </div>
+                                                </div>
+                                                <div className="alert_icon">
+                                                    <div className="alert">
+                                                        </div>
+                                                </div>
                                             <button type="submit" className="view_button">View Department</button>
                                         </div>
                                 </div>
