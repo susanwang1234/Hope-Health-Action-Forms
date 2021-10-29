f74d758d7b3c8737f7d6c17c9dc2846400385e23
Matt Fung <chf2@sfu.ca>
2021-10-28 21:55:16 -0700

    68: Add dummy department for admins

--- ceres-server/src/test/testDepartmentForm.spec.ts
+++ ceres-server/src/test/testDepartmentForm.spec.ts
@@ -55,7 +55,7 @@ describe('testGetDepartmentFormFailure', () => {
   it('Throw error code 404 for department form yet to be created', (done) => {
     chai
       .request(testApp)
-      .get('/department_form/3')
+      .get('/department_form/4')
       .end((err: any, res: any) => {
         expect(err).to.be.null;
         expect(res).to.have.status(404);
@@ -79,50 +79,6 @@ describe('testGetDepartmentFormSuccess', () => {
   });
   after('Close a working server', () => {
     httpServer.close();
-  });
-  it('Validate department form #1 request properties', (done) => {
-    chai
-      .request(testApp)
-      .get('/department_form/1')
-      .end((err: any, res: any) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an('array');
-        res.body.forEach((item: any) => {
-          expect(item).to.be.an('object');
-          expect(item).to.have.deep.property('id');
-          expect(item).to.have.deep.property('name');
-          expect(item).to.have.deep.property('label');
-          expect(item).to.have.deep.property('inputType');
-          expect(item).to.have.deep.property('responseType');
-          expect(item).to.have.deep.property('isMSPP');
-          expect(item).to.have.deep.property('departmentId');
-          expect(item).to.have.deep.property('questionId');
-          expect(item).to.have.deep.property('isRequired');
-        });
-        done();
-      });
-  });
-  it('Validate department form #1 request fields', (done) => {
-    chai
-      .request(testApp)
-      .get('/department_form/1')
-      .end((err: any, res: any) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        res.body.forEach((item: any) => {
-          expect(item.id).to.deep.equal(++id);
-          expect(item.name).to.deep.equal('Rehab');
-          expect(item.label).to.deep.equal(labels[id - 1]);
-          expect(item.inputType).to.deep.equal('text');
-          expect(item.responseType).to.deep.equal('number');
-          expect(item.isMSPP).to.deep.equal(1);
-          expect(item.departmentId).to.deep.equal(1);
-          expect(item.questionId).to.deep.equal(id);
-          expect(item.isRequired).to.deep.equal(1);
-        });
-        done();
-      });
   });
   it('Validate department form #2 request properties', (done) => {
     chai
@@ -156,12 +112,56 @@ describe('testGetDepartmentFormSuccess', () => {
         expect(res).to.have.status(200);
         res.body.forEach((item: any) => {
           expect(item.id).to.deep.equal(++id);
+          expect(item.name).to.deep.equal('Rehab');
+          expect(item.label).to.deep.equal(labels[id - 1]);
+          expect(item.inputType).to.deep.equal('text');
+          expect(item.responseType).to.deep.equal('number');
+          expect(item.isMSPP).to.deep.equal(1);
+          expect(item.departmentId).to.deep.equal(2);
+          expect(item.questionId).to.deep.equal(id);
+          expect(item.isRequired).to.deep.equal(1);
+        });
+        done();
+      });
+  });
+  it('Validate department form #3 request properties', (done) => {
+    chai
+      .request(testApp)
+      .get('/department_form/3')
+      .end((err: any, res: any) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        res.body.forEach((item: any) => {
+          expect(item).to.be.an('object');
+          expect(item).to.have.deep.property('id');
+          expect(item).to.have.deep.property('name');
+          expect(item).to.have.deep.property('label');
+          expect(item).to.have.deep.property('inputType');
+          expect(item).to.have.deep.property('responseType');
+          expect(item).to.have.deep.property('isMSPP');
+          expect(item).to.have.deep.property('departmentId');
+          expect(item).to.have.deep.property('questionId');
+          expect(item).to.have.deep.property('isRequired');
+        });
+        done();
+      });
+  });
+  it('Validate department form #3 request fields', (done) => {
+    chai
+      .request(testApp)
+      .get('/department_form/3')
+      .end((err: any, res: any) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        res.body.forEach((item: any) => {
+          expect(item.id).to.deep.equal(++id);
           expect(item.name).to.deep.equal('NICUPaeds');
           expect(item.label).to.deep.equal(labels[id - 1 - 13]);
           expect(item.inputType).to.deep.equal('text');
           expect(item.responseType).to.deep.equal('number');
           expect(item.isMSPP).to.deep.equal(1);
-          expect(item.departmentId).to.deep.equal(2);
+          expect(item.departmentId).to.deep.equal(3);
           expect(item.questionId).to.deep.equal(id - 13);
           expect(item.isRequired).to.deep.equal(1);
         });
