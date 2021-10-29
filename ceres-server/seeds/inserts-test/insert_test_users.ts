94fb5208ea132893740dd05333f9e272dcde00ba
Matt Fung <chf2@sfu.ca>
2021-10-28 20:08:21 -0700

    68: Create seed for users

--- ceres-server/seeds/inserts-test/insert_test_users.ts
+++ ceres-server/seeds/inserts-test/insert_test_users.ts
@@ -1,6 +1,6 @@
 import { Knex } from 'knex';
 
-export async function insertUsers(knex: Knex): Promise<void> {
+export async function insertTestUsers(knex: Knex): Promise<void> {
   await knex('User').insert([{ id: 1, username: 'admin', password: '$2b$12$kUy4kEGLkdmB9hgSxtyOYetqixdHXOWOa/OSNKcYopCZVhQogwjOm', departmentId: 1, roleId: 1 }]);
   await knex('User').insert([{ id: 2, username: 'staff01', password: '$2a$12$Es2.DjFL5jKSukJjgXOubuP..MipNRcqM5KfzL49bdymFqAkB62r2', departmentId: 2, roleId: 4 }]);
 }
