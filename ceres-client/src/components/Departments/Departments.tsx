10d4d5c52ec72ba023841063f2bf4b7c46765473
Aida Kadiric <akadiric@sfu.ca>
2021-10-29 17:30:09 -0700

    Buttons navigate to dashboard

--- ceres-client/src/components/Departments/Departments.tsx
+++ ceres-client/src/components/Departments/Departments.tsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import DepartmentData from "./Departments.json";
 import ToDoData from "./ToDo.json";
 import './Departments.css';
+import { useHistory } from 'react-router-dom';
 //source for checkmark icon:https://css.gg/check-o
 //source for alert icon: https://css.gg/danger
 /*
@@ -10,7 +11,7 @@ if (DepartmentDetail.name == "Maternity") {
 </div>
 }
 */
-
+/*
 class Departments extends Component {
     render() {
         return (
@@ -24,10 +25,12 @@ class Departments extends Component {
                                                     <div className="checkmark">
                                                         </div>
                                                 </div>
+                                                <h4>Case Study</h4>
                                                 <div className="alert_icon">
                                                     <div className="alert">
                                                         </div>
                                                 </div>
+                                                <h4>MSPP Report</h4>
                                             <button type="submit" className="view_button">View Department</button>
                                         </div>
                                 </div>
@@ -38,5 +41,38 @@ class Departments extends Component {
         )
     }
 }
+*/
+function Departments(){
+    let history = useHistory();
 
+    const onClick = () => {
+        history.push('/dashboard');
+    };
+    return (
+        <div className="card_container">
+            <div className="cards">
+                        {DepartmentData.map((DepartmentDetail, index)=>{
+                        return <div className="card">
+                                    <div className="card_inner">
+                                        <h2>{DepartmentDetail.name}</h2>
+                                            <div className="checkmark_icon">
+                                                <div className="checkmark">
+                                                    </div>
+                                            </div>
+                                            <h4>Case Study</h4>
+                                            <div className="alert_icon">
+                                                <div className="alert">
+                                                    </div>
+                                            </div>
+                                            <h4>MSPP Report</h4>
+                                        <button type="submit" onClick={onClick} className="view_button">View Department</button>
+                                    </div>
+                            </div>
+                   })}
+            </div>
+        </div>
+
+    )
+
+}
 export default Departments
