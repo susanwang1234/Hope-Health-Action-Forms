9bc6f2e8b06ed81c3ef4c3069aed12f5a3c4825f
Aida Kadiric <akadiric@sfu.ca>
2021-10-29 22:51:46 -0700

    Function for dynamic icons

--- ceres-client/src/components/Departments/Departments.tsx
+++ ceres-client/src/components/Departments/Departments.tsx
@@ -5,74 +5,43 @@ import './Departments.css';
 import { useHistory } from 'react-router-dom';
 //source for checkmark icon:https://css.gg/check-o
 //source for alert icon: https://css.gg/danger
-/*
-if (DepartmentDetail.name == "Maternity") {
-<div className="checkmark_icon">
-</div>
-}
-*/
-/*
-class Departments extends Component {
-    render() {
-        return (
-            <div className="card_container">
-                <div className="cards">
-                            {DepartmentData.map((DepartmentDetail, index)=>{
-                            return <div className="card">
-                                        <div className="card_inner">
-                                            <h2>{DepartmentDetail.name}</h2>
-                                                <div className="checkmark_icon">
-                                                    <div className="checkmark">
-                                                        </div>
-                                                </div>
-                                                <h4>Case Study</h4>
-                                                <div className="alert_icon">
-                                                    <div className="alert">
-                                                        </div>
-                                                </div>
-                                                <h4>MSPP Report</h4>
-                                            <button type="submit" className="view_button">View Department</button>
-                                        </div>
-                                </div>
-                       })}
-                </div>
-            </div>
 
-        )
-    }
-}
-*/
 function Departments(){
     let history = useHistory();
 
     const onClick = () => {
         history.push('/dashboard');
     };
+
+    function iconChecker(index: number){
+        if(ToDoData[index].caseStudy){
+            return (
+                <div className="checkmark_icon">
+                    <div className="checkmark"></div>
+                </div>
+            );}
+        return(
+                <div className="alert_icon">
+                    <div className="alert"></div>
+                </div>
+        );
+    }
     return (
-        <div className="card_container">
-            <div className="cards">
+        <div className="background">
+            <div className="card_container">
+                <div className="cards">
                         {DepartmentData.map((DepartmentDetail, index)=>{
-                        return <div className="card">
-                                    <div className="card_inner">
-                                        <h2>{DepartmentDetail.name}</h2>
-                                            <div className="checkmark_icon">
-                                                <div className="checkmark">
-                                                    </div>
-                                            </div>
-                                            <h4>Case Study</h4>
-                                            <div className="alert_icon">
-                                                <div className="alert">
-                                                    </div>
-                                            </div>
-                                            <h4>MSPP Report</h4>
+                            return <div className="card">
+                                        <div className="card_inner">
+                                            <h2><b>{DepartmentDetail.name}</b></h2>
+                                            {iconChecker(index)}
                                         <button type="submit" onClick={onClick} className="view_button">View Department</button>
                                     </div>
-                            </div>
-                   })}
+                                </div>
+                        })}
+                </div>
             </div>
         </div>
-
     )
-
 }
 export default Departments
