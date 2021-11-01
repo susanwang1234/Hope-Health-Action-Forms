d0820195ec27a98fbfe9542b72ab09bac7bb586c
ysaleem <ysaleem@sfu.ca>
2021-11-01 15:08:39 -0700

    Implemented sidebar dropdown button

--- ceres-client/src/components/Sidebar/Sidebar.tsx
+++ ceres-client/src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,8 @@
 /*Citation: https://github.com/mustafaerden/react-admin-dashboard*/
 
 import { Link, useLocation } from 'react-router-dom'
-import React from 'react';
-import { Nav } from 'react-bootstrap';
+import React, {useState} from 'react';
+import { Button, Nav } from 'react-bootstrap';
 import { IconContext} from 'react-icons';
 import { AiOutlineHome } from 'react-icons/ai';
 import { setSyntheticLeadingComments } from 'typescript';
@@ -10,9 +10,13 @@ import '../../App.css';
 import {HiHome, HiStar} from 'react-icons/hi'
 import {BsFillPersonFill, BsPencilSquare} from 'react-icons/bs'
 import {IoIosJournal} from 'react-icons/io'
+import {IoMdArrowDropdown} from 'react-icons/io'
+import './Sidebar.css'
 
 
 const Sidebar = ({show}:any) => {
+
+  const [open, setOpen] = useState(false);
 
   return (
     <div className={show ? 'sidebar active' : 'sidebar'}>
@@ -22,16 +26,23 @@ const Sidebar = ({show}:any) => {
           <Link to="/dashboard" className='sideLink'><HiHome/>Home</Link>
         </li>
         <li>
+          <Link to="/dashboard" className='sideLink'><BsPencilSquare/>Departments</Link>
+        </li>
+        <li>
           <Link to="/dashboard" className='sideLink'><BsPencilSquare/>Reports</Link>
         </li>
         <li>
-          <Link to="/" className='sideLink'><IoIosJournal/>Case Study</Link>
+          <Link to="/dashboard" className='sideLink'><IoIosJournal/>Case Studies</Link>
         </li>
         <li>
-          <Link to="/" className='sideLink'><HiStar/>Leaderboard</Link>
-        </li>
-        <li>
-          <Link to="/" className='sideLink'><BsFillPersonFill/>Employee of the Month</Link>
+          <Button className='button' onClick={() => setOpen(!open)}><IoMdArrowDropdown/>Reports</Button>
+          {open && 
+            <li>
+              <Link to="/dashboard" className='link' color='red'><IoIosJournal/>This Month's Report</Link>
+              <Link to="/dashboard" className='link'><IoIosJournal/>Previous Reports</Link>
+              <Link to="/dashboard" className='link'><IoIosJournal/>Statistics</Link>
+            </li>
+                }
         </li>
       </ul>
     </div>
