f645812fc3da0086a72f6623f032269bc6647e00
rgoyal <ritika_goyal@sfu.ca>
2021-10-31 22:48:00 -0700

    made top bar with logo

--- ceres-client/src/components/CaseStudy/CaseStudySubmit.tsx
+++ ceres-client/src/components/CaseStudy/CaseStudySubmit.tsx
@@ -3,6 +3,7 @@ import Navbar from '../Navbar/Navbar';
 import { useHistory } from 'react-router-dom';
 import React, { useContext } from 'react';
 import { UserContext } from '../../UserContext';
+import logo from '../../images/navlogo.png';
 
 import data_dummy from './DummyDataCS.json'
 
@@ -14,10 +15,37 @@ const CaseStudy = () => {
   console.log('Username (Case Study) is ' , userContext.user?.name)
   console.log('Department (Case Study) is ' , userContext.user?.department)
   return (
-    <main>
-      <Navbar />
-      <div className="flex">
+    <div className= "w-full h-full flex flex-col">
+      <div className="headerbar">
+        <img src={logo} className="department_logo"></img>
+        <button type="submit" className="logout_button">Log Out</button>
+      </div>
+      <div className="flex w-full flex-col h-screen justify-center items-center bg-gray-100">
+        <div>
+          abcd
+        </div>
+        <div className = "flex flex-col w-8/12 shadow-2xl h-8/12 border p-8 bg-white">
+          <h1 className = "text-center">Current Case Study</h1>
+          <div className = "flex flex-col">
+            <h2 className = "mb-4">Type of Case Study</h2>
+            <input placeholder = "Dropdown"/>
+
+
+          </div>
+        </div>    
+      </div>
+    </div>
+
+  );
+};
+
+export default CaseStudy;
+
+
+  {/* 
         <h1>Current Case Study</h1>
+
+        
         {data_dummy.map((Story, index) => {
           return <div>
             <h1>{Story.name}</h1>
@@ -32,10 +60,4 @@ const CaseStudy = () => {
             
             })}</h2>
           </div>
-        })}
-      </div>
-    </main>
-  );
-};
-
-export default CaseStudy;
+        })} */}
