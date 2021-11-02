f651e44a17ac9404f9e4cd896f7b0812056def73
Keegan McGinnis <kmcginni@sfu.ca>
2021-11-01 23:42:29 -0700

    40: Change new form controller to use templates

--- ceres-server/src/controllers/formController.ts
+++ ceres-server/src/controllers/formController.ts
@@ -1,31 +1,22 @@
 import logging from '../config/logging';
 import { Request, Response, NextFunction } from 'express';
 import { Knex } from '../db/mysql';
+import { createItem } from './requestTemplates/createRequest';
+import { isInvalidInput } from './requestTemplates/isInvalidInput';
+import { departmentNegativeOrNanInputError } from 'shared/errorMessages';
 
 const NAMESPACE = 'Form';
+const TABLE_NAME = 'Form';
+// TODO: Implement better error catching, start by using this for foreign key constraint errors
 const SQL_FOREIGN_KEY_CONSTRAINT_ERROR_CODE: number = 1452;
 
 const createNewForm = async (req: Request, res: Response, next: NextFunction) => {
-  logging.info(NAMESPACE, 'Creating new form');
   const departmentId: number = +req.body.departmentId;
-  if (!departmentId || departmentId < 0) {
-    res.status(400).send({ error: 'Incorrect usage for POST /form, department_id must be a positive integer' });
+  if (isInvalidInput(departmentId)) {
+    res.status(400).send(departmentNegativeOrNanInputError);
     return;
   }
-
-  try {
-    const newFormId = await Knex('Form').insert({ departmentId: departmentId });
-    const newForm = await Knex.select('*').from('Form').where('id', newFormId);
-    logging.info(NAMESPACE, `Created new form for department ${departmentId}`, newForm);
-    res.status(201).send(newForm);
-  } catch (error: any) {
-    logging.error(NAMESPACE, error.message, error);
-    if (error.errno === SQL_FOREIGN_KEY_CONSTRAINT_ERROR_CODE) {
-      res.status(404).send({ error: `Cannot find department with id ${departmentId}` });
-      return;
-    }
-    res.status(500).send(error);
-  }
+  await createItem(req, res, next, NAMESPACE, TABLE_NAME, req.body);
 };
 
 export default { createNewForm };
--- ceres-server/src/shared/errorMessages.ts
+++ ceres-server/src/shared/errorMessages.ts
@@ -6,6 +6,7 @@ function dneError(entity: string, errorMessage: string) {
   return { error: `${entity} ${errorMessage}` };
 }
 
+// TODO: Generalize these better so they can be used for more controllers
 const departmentNegativeOrNanInputError = negativeOrNanInputError('/departmentForm/:id');
 const departmentDNEError = dneError('Department', 'does not exist or its form structure is empty');
 const userNegativeOrNanInputError = negativeOrNanInputError('/user/:id');
