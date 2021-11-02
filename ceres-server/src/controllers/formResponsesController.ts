7c30889535c6c31763d5d1b6534f1b9625aa845b
Keegan McGinnis <kmcginni@sfu.ca>
2021-11-02 16:38:59 -0700

    40: Add function in post template for posting array of multiple items and retrieving them

--- ceres-server/src/controllers/formResponsesController.ts
+++ ceres-server/src/controllers/formResponsesController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { createItem } from './requestTemplates/createRequest';
+import { createItems } from './requestTemplates/createRequest';
 
 const NAMESPACE = 'Form Response';
 const TABLE_NAME = 'FormResponse';
@@ -9,7 +9,8 @@ const addNewFormResponses = async (req: Request, res: Response, next: NextFuncti
   const formResponses = req.body.map((formResponse: any) => {
     return { ...formResponse, formId: formId };
   });
-  await createItem(req, res, next, NAMESPACE, TABLE_NAME, formResponses);
+  const formResponseFKName = 'formID';
+  await createItems(req, res, next, NAMESPACE, TABLE_NAME, formResponses, formResponseFKName, formId);
 };
 
 export default { addNewFormResponses };
