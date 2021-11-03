4f0a3070f002de03c1723fa7c57fa7a5199643c5
Keegan McGinnis <kmcginni@sfu.ca>
2021-11-03 13:05:41 -0700

    40: Create test for successful form creation

--- ceres-server/seeds/seed_test.ts
+++ ceres-server/seeds/seed_test.ts
@@ -8,13 +8,17 @@ import { insertTestUsers } from './inserts-test/insert_test_users';
 
 export async function seed(knex: Knex): Promise<void> {
   // Delete all existing entries, keeping in mind of foreign key constraints
+  await knex.raw('SET FOREIGN_KEY_CHECKS=0;');
   await knex('Dummies').del();
   await knex('Rehab_Report').del();
   await knex('User').del();
   await knex('Role').del();
+  await knex('FormResponse').del();
+  await knex('Form').truncate();
   await knex('DepartmentQuestion').del();
   await knex('Department').del();
   await knex('Question').del();
+  await knex.raw('SET FOREIGN_KEY_CHECKS=1;');
 
   // Insert seed entries
   await insertTestQuestions(knex);
