e6296ac0e0c883e33e903be047b233bc2accb8d5
Keegan McGinnis <kmcginni@sfu.ca>
2021-11-04 00:04:09 -0700

    73: Add seed inserts to seed for test files

--- ceres-server/seeds/seed_test.ts
+++ ceres-server/seeds/seed_test.ts
@@ -6,9 +6,13 @@ import { insertTestNICUPaedsDepartmentQuestions } from './inserts-test/insert_te
 import { insertTestRoles } from './inserts-test/insert_test_roles';
 import { insertTestUsers } from './inserts-test/insert_test_users';
 import { insertTestCaseStudyTypes } from './inserts-test/insert_test_case_study_types';
+import { insertTestCaseStudyQuestions } from './inserts-test/insert_test_case_study_questions';
+import { insertTestCaseStudyTypeQuestions } from './inserts-test/insert_test_case_study_type_questions';
 
 export async function seed(knex: Knex): Promise<void> {
   // Delete all existing entries, keeping in mind of foreign key constraints
+  await knex('CaseStudyTypeQuestion').del();
+  await knex('CaseStudyQuestion').del();
   await knex('CaseStudyType').del();
   await knex('Dummies').del();
   await knex('Rehab_Report').del();
@@ -26,4 +30,6 @@ export async function seed(knex: Knex): Promise<void> {
   await insertTestRoles(knex);
   await insertTestUsers(knex);
   await insertTestCaseStudyTypes(knex);
+  await insertTestCaseStudyQuestions(knex);
+  await insertTestCaseStudyTypeQuestions(knex);
 }
