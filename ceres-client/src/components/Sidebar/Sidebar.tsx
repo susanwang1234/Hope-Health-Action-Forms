8d29524ac53809786c2f273db2be6b0fae26bee4
ysaleem <ysaleem@sfu.ca>
2021-11-04 01:45:46 -0700

    Refactoring of css class names\n Removing unused imports

--- ceres-client/src/components/Sidebar/Sidebar.tsx
+++ ceres-client/src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,10 @@
 /*Citation: https://github.com/mustafaerden/react-admin-dashboard*/
 
-import { Link, useLocation } from 'react-router-dom';
-import React, { useState } from 'react';
-import { Button, Nav } from 'react-bootstrap';
-import { IconContext } from 'react-icons';
-import { AiOutlineHome } from 'react-icons/ai';
-import { setSyntheticLeadingComments } from 'typescript';
-import { HiHome, HiStar } from 'react-icons/hi';
-import { BsFillPersonFill, BsPencilSquare, BsFillCalendarFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { HiHome} from 'react-icons/hi';
+import { BsPencilSquare, BsFillCalendarFill } from 'react-icons/bs';
 import { IoIosJournal } from 'react-icons/io';
 import { IoMdArrowDropdown } from 'react-icons/io';
 import { FaHistory } from 'react-icons/fa'
@@ -21,39 +18,39 @@ const Sidebar = ({ show }: any) => {
     <div className={show ? 'sidebar active' : 'sidebar'}>
       <ul>
         <li>
-          <Link to="/dashboard" className="sideLink">
+          <Link to="/dashboard" className="side-link">
             <HiHome />
             Home
           </Link>
         </li>
         <li>
-          <Link to="/departments" className="sideLink">
+          <Link to="/departments" className="side-link">
             <BsPencilSquare />
             Departments
           </Link>
         </li>
         <li>
-          <Link to="/case-study" className="sideLink">
+          <Link to="/case-study" className="side-link">
             <IoIosJournal />
             Case Studies
           </Link>
         </li>
         <li>
-          <Button className="sideButton" onClick={() => setOpen(!open)}>
+          <Button className="side-button" onClick={() => setOpen(!open)}>
             <IoMdArrowDropdown />
             Reports
           </Button>
           {open && (
             <li>
-              <Link to="/submit-report" className="dropLink" color="red">
+              <Link to="/submit-report" className="drop-link" color="red">
                 <BsFillCalendarFill />
                 This Month's Report
               </Link>
-              <Link to="/data-page" className="dropLink">
+              <Link to="/data-page" className="drop-link">
                 <FaHistory />
                 Previous Reports
               </Link>
-              <Link to="/statistics" className="dropLink">
+              <Link to="/statistics" className="drop-link">
                 <ImStatsDots />
                 Statistics
               </Link>
