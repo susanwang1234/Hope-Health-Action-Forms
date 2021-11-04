2e956b06ecaaf7c5d846590d4f8d040097f02709
Matt Fung <chf2@sfu.ca>
2021-11-04 15:13:08 -0700

    Refactor getMonthAndTitle

--- ceres-client/src/components/Forms/Forms.tsx
+++ ceres-client/src/components/Forms/Forms.tsx
@@ -8,7 +8,7 @@ import Element from './Elements';
 import { FormContext } from './FormContext';
 import JSONfile from './jsonForms/rehabForm.json';
 import '../../App.css';
-import ToggleSwitch from './ToggleSwitch'
+import ToggleSwitch from './ToggleSwitch';
 
 // console.log('JSONfile', JSONfile);
 
@@ -57,26 +57,12 @@ function Forms() {
     console.log(elements);
   };
 
-
   const getMonthAndTitle: any = (part_of_title: any) => {
     //Citation: https://www.w3schools.com/jsref/jsref_getmonth.asp
-    const month = new Array();
-    month[0] = "JANUARY";
-    month[1] = "FEBRUARY";
-    month[2] = "MARCH";
-    month[3] = "APRIL";
-    month[4] = "MAY";
-    month[5] = "JUNE";
-    month[6] = "JULY";
-    month[7] = "AUGUST";
-    month[8] = "SEPTEMBER";
-    month[9] = "OCTOBER";
-    month[10] = "NOVEMBER";
-    month[11] = "DECEMBER";
-
+    const month = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER'];
     const month_index = new Date().getMonth();
     let nameOfMonth = month[month_index];
-    return nameOfMonth + " " + part_of_title;
+    return nameOfMonth + ' ' + part_of_title;
   };
 
   return (
@@ -84,15 +70,23 @@ function Forms() {
       <main>
         <Navbar />
         <React.Fragment>
-        <ToggleSwitch label="MSPP Data only" />
+          <ToggleSwitch label="MSPP Data only" />
         </React.Fragment>
         <div>
           <div className="blocks-form">
-          <div className="title-form"><Form.Label column="lg">{getMonthAndTitle(page_label)}</Form.Label></div>
+            <div className="title-form">
+              <Form.Label column="lg">{getMonthAndTitle(page_label)}</Form.Label>
+            </div>
             <Form>
               {fields ? fields.map((my_field: any, my_key: any) => <Element key={my_key} field={my_field} />) : null}
               <div className="button-form">
-                <button className="view-cancel-form-button" onClick={(e) => {e.preventDefault(); window.location.href='/dashboard';}}>
+                <button
+                  className="view-cancel-form-button"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href = '/dashboard';
+                  }}
+                >
                   Cancel
                 </button>
                 <button className="view-submit-form-button" onClick={(e) => handleSave(e)}>
@@ -101,11 +95,9 @@ function Forms() {
               </div>
             </Form>
           </div>
-
         </div>
       </main>
     </FormContext.Provider>
-    
   );
 }
 
