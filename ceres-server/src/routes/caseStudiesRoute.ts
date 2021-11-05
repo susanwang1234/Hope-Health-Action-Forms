3a48b2d06653d153643bf4ee131e5e8055bc4caa
Matt Fung <chf2@sfu.ca>
2021-11-05 02:48:26 -0700

    78: Add get request for fetching a case study

--- ceres-server/src/routes/caseStudiesRoute.ts
+++ ceres-server/src/routes/caseStudiesRoute.ts
@@ -4,5 +4,6 @@ import controller from '../controllers/caseStudiesController';
 const router = express.Router();
 
 router.get('', controller.getCaseStudies);
+router.get('/:id', controller.getCaseStudyById);
 
 export = router;
--- ceres-server/src/shared/errorMessages.ts
+++ ceres-server/src/shared/errorMessages.ts
@@ -6,10 +6,12 @@ function dneError(entity: string, errorMessage: string) {
   return { error: `${entity} ${errorMessage}` };
 }
 
-const departmentNegativeOrNanInputError = negativeOrNanInputError('/departmentForm/:id');
+const caseStudyNegativeOrNanInputError = negativeOrNanInputError('/case-studies/:id');
+const caseStudyDNEError = dneError('Case Study', 'does not exist');
+const departmentNegativeOrNanInputError = negativeOrNanInputError('/department-form/:id');
 const departmentDNEError = dneError('Department', 'does not exist or its form structure is empty');
 const userNegativeOrNanInputError = negativeOrNanInputError('/user/:id');
 const userDNEError = dneError('User', 'does not exist');
 const pageNotFoundError = { message: 'not found' };
 
-export { departmentNegativeOrNanInputError, departmentDNEError, userNegativeOrNanInputError, userDNEError, pageNotFoundError };
+export { caseStudyNegativeOrNanInputError, caseStudyDNEError, departmentNegativeOrNanInputError, departmentDNEError, userNegativeOrNanInputError, userDNEError, pageNotFoundError };
