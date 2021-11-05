3c69193b970c1bdab8c2ff9c43146bad89545e66
Matt Fung <chf2@sfu.ca>
2021-11-05 03:01:17 -0700

    78 Fix errors with logging.info

--- ceres-server/src/controllers/caseStudiesController.ts
+++ ceres-server/src/controllers/caseStudiesController.ts
@@ -8,14 +8,14 @@ const NAMESPACE = 'Case Studies Control';
 const TABLE_NAME = 'Case Studies';
 
 const getCaseStudies = async (req: Request, res: Response, next: NextFunction) => {
-  logging.info(NAMESPACE, `GETTING LIST OF ${TABLE_NAME.toUpperCase}`);
+  logging.info(NAMESPACE, `GETTING LIST OF ${TABLE_NAME.toUpperCase()}`);
   try {
     const retrievedCaseStudies = await Knex.select('CaseStudy.*', 'CaseStudyResponse.response')
       .from('CaseStudy')
       .join('CaseStudyResponse', 'CaseStudy.id', '=', 'CaseStudyResponse.caseStudyId')
       .join('CaseStudyTypeQuestion', 'CaseStudyResponse.caseStudyTypeQuestionId', '=', 'CaseStudyTypeQuestion.id')
       .where('CaseStudyTypeQuestion.caseStudyQuestionId', '=', '7');
-    logging.info(NAMESPACE, `Retrieved ${TABLE_NAME.toUpperCase}:`, retrievedCaseStudies);
+    logging.info(NAMESPACE, `RETRIEVED ${TABLE_NAME.toUpperCase()}:`, retrievedCaseStudies);
     res.status(200).send(retrievedCaseStudies);
   } catch (error: any) {
     logging.error(NAMESPACE, error.message, error);
@@ -24,7 +24,7 @@ const getCaseStudies = async (req: Request, res: Response, next: NextFunction) =
 };
 
 const getCaseStudyById = async (req: Request, res: Response, next: NextFunction) => {
-  logging.info(NAMESPACE, `GETTING A ${TABLE_NAME.toUpperCase} BY ID`);
+  logging.info(NAMESPACE, `GETTING A ${TABLE_NAME.toUpperCase()} BY ID`);
   const caseStudyId: number = +req.params.id;
   if (isInvalidInput(caseStudyId)) {
     res.status(400).send(caseStudyNegativeOrNanInputError);
@@ -39,7 +39,7 @@ const getCaseStudyById = async (req: Request, res: Response, next: NextFunction)
       .join('CaseStudyQuestion', 'CaseStudyTypeQuestion.caseStudyQuestionId', '=', 'CaseStudyQuestion.id')
       .join('CaseStudyType', 'CaseStudyTypeQuestion.caseStudyTypeId', '=', 'CaseStudyType.id')
       .where('CaseStudy.id', '=', caseStudyId);
-    logging.info(NAMESPACE, `GOT ${TABLE_NAME.toUpperCase} FOR DEPARTMENT ${caseStudyId}`, retrievedCaseStudy);
+    logging.info(NAMESPACE, `RETRIEVED CASE STUDY ${caseStudyId}`, retrievedCaseStudy);
     if (!retrievedCaseStudy.length) {
       res.status(404).send(caseStudyDNEError);
       return;
--- ceres-server/src/controllers/requestTemplates/getAllRequest.ts
+++ ceres-server/src/controllers/requestTemplates/getAllRequest.ts
@@ -6,10 +6,10 @@ import { Role } from '../../db/models/roleModel';
 import { Department } from 'db/models/departmentModel';
 
 export const getItems = async (req: Request, res: Response, next: NextFunction, NAMESPACE: string, TABLE_NAME: string) => {
-  logging.info(NAMESPACE, `GETTING LIST OF ${TABLE_NAME.toUpperCase}`);
+  logging.info(NAMESPACE, `GETTING LIST OF ${TABLE_NAME.toUpperCase()}`);
   try {
     const items: User[] | Role[] | Department[] = await Knex.select('*').from(TABLE_NAME);
-    logging.info(NAMESPACE, `Retrieved ${TABLE_NAME.toUpperCase}:`, items);
+    logging.info(NAMESPACE, `RETRIEVED ${TABLE_NAME.toUpperCase()}S:`, items);
     res.status(200).send(items);
   } catch (error: any) {
     logging.error(NAMESPACE, error.message, error);
