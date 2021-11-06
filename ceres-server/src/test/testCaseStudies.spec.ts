5606fb8632490591eb18a9444c874f27cfac05d0
Matt Fung <chf2@sfu.ca>
2021-11-05 20:53:00 -0700

    78: Add test for getting case study questions by id

--- ceres-server/src/test/testCaseStudies.spec.ts
+++ ceres-server/src/test/testCaseStudies.spec.ts
@@ -2,7 +2,7 @@ import http from 'http';
 import { createServer, enableRoutes, sendFirstRequest } from '../server';
 import { Application } from 'express';
 import PORT from './testTools/serverPort';
-import { caseStudyNegativeOrNanInputError, caseStudyDNEError } from 'shared/errorMessages';
+import { caseStudyNegativeOrNanInputError, caseStudyDNEError, caseStudyQuestionsNegativeOrNanInputError, caseStudyQuestionsDNEError } from 'shared/errorMessages';
 const expect = require('chai').expect;
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -102,7 +102,7 @@ describe('testGetCaseStudyFailure', () => {
         done();
       });
   });
-  it('Throw error code 404 for user yet to be created', (done) => {
+  it('Throw error code 404 for case study yet to be created', (done) => {
     chai
       .request(testApp)
       .get('/case-studies/55')
@@ -115,7 +115,7 @@ describe('testGetCaseStudyFailure', () => {
   });
 });
 
-// Test 2: GET request (Success, single case study)
+// Test 3: GET request (Success, single case study)
 describe('testGetCaseStudySuccess', () => {
   let testApp: Application;
   let httpServer: http.Server;
@@ -176,7 +176,7 @@ describe('testGetCaseStudySuccess', () => {
   });
 });
 
-// Test 3: GET request (List of case study types)
+// Test 4: GET request (List of case study types)
 describe('testGetCaseStudyTypesSuccess', () => {
   let testApp: Application;
   let httpServer: http.Server;
@@ -218,6 +218,115 @@ describe('testGetCaseStudyTypesSuccess', () => {
         res.body.forEach((item: any) => {
           expect(item.id).to.deep.equal(++id);
           expect(item.name).to.deep.equal(caseStudyTypes[id - 1]);
+        });
+        done();
+      });
+  });
+});
+
+// Test 5: GET request (Failure, List of case study questions)
+describe('testGetCaseStudyQuestionsFailure', () => {
+  let testApp: Application;
+  let httpServer: http.Server;
+  before('Create a working server', () => {
+    testApp = createServer();
+    sendFirstRequest(testApp);
+    enableRoutes(testApp);
+    httpServer = http.createServer(testApp);
+    httpServer.listen(PORT);
+  });
+  after('Close a working server', () => {
+    httpServer.close();
+  });
+  it('Throw error code 400 for user with a negative number', (done) => {
+    chai
+      .request(testApp)
+      .get('/case-study-questions/-1')
+      .end((err: any, res: any) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.deep.equal(JSON.stringify(caseStudyQuestionsNegativeOrNanInputError));
+        done();
+      });
+  });
+  it('Throw error code 400 for invalid URL', (done) => {
+    chai
+      .request(testApp)
+      .get('/case-study-questions/wow')
+      .end((err: any, res: any) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res.text).to.deep.equal(JSON.stringify(caseStudyQuestionsNegativeOrNanInputError));
+        done();
+      });
+  });
+  it('Throw error code 404 for case study questions yet to be created', (done) => {
+    chai
+      .request(testApp)
+      .get('/case-study-questions/6')
+      .end((err: any, res: any) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(404);
+        expect(res.text).to.deep.equal(JSON.stringify(caseStudyQuestionsDNEError));
+        done();
+      });
+  });
+});
+
+// Test 6: GET request (Success, List of case study questions)
+describe('testGetCaseStudyQuestionsSuccess', () => {
+  let testApp: Application;
+  let httpServer: http.Server;
+  const label = ['Training date?', 'What was the training on?', 'Who conducted the training?', 'Who attended the training?', 'How will the training benefit HCBH and its staff?', 'Story'];
+  const caseStudyQuestionId = [13, 14, 15, 16, 17, 7];
+  const inputType = ['text', 'text', 'text', 'text', 'text', 'textarea'];
+  let id = 13;
+  before('Create a working server', () => {
+    testApp = createServer();
+    sendFirstRequest(testApp);
+    enableRoutes(testApp);
+    httpServer = http.createServer(testApp);
+    httpServer.listen(PORT);
+  });
+  after('Close a working server', () => {
+    httpServer.close();
+  });
+  it('Validate the case study questions request properties', (done) => {
+    chai
+      .request(testApp)
+      .get('/case-study-questions/3')
+      .end((err: any, res: any) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        res.body.forEach((item: any) => {
+          expect(item).to.be.an('object');
+          expect(item).to.have.deep.property('name');
+          expect(item).to.have.deep.property('caseStudyTypeQuestionId');
+          expect(item).to.have.deep.property('caseStudyTypeId');
+          expect(item).to.have.deep.property('caseStudyQuestionId');
+          expect(item).to.have.deep.property('label');
+          expect(item).to.have.deep.property('inputType');
+          expect(item).to.have.deep.property('responseType');
+        });
+        done();
+      });
+  });
+  it('Validate the case study questions request fields', (done) => {
+    chai
+      .request(testApp)
+      .get('/case-study-questions/3')
+      .end((err: any, res: any) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        res.body.forEach((item: any) => {
+          expect(item.name).to.deep.equal('Training Session');
+          expect(item.caseStudyTypeQuestionId).to.deep.equal(++id);
+          expect(item.caseStudyTypeId).to.deep.equal(3);
+          expect(item.caseStudyQuestionId).to.deep.equal(caseStudyQuestionId[id - 14]);
+          expect(item.label).to.deep.equal(label[id - 14]);
+          expect(item.inputType).to.deep.equal(inputType[id - 14]);
+          expect(item.responseType).to.deep.equal('string');
         });
         done();
       });
