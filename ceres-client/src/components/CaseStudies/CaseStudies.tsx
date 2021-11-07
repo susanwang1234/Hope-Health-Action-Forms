44101e2167aced0b84016ad6a55f73c35015af9f
ysaleem <ysaleem@sfu.ca>
2021-11-07 15:49:21 -0800

    Added button onClick functionality

--- ceres-client/src/components/CaseStudies/CaseStudies.tsx
+++ ceres-client/src/components/CaseStudies/CaseStudies.tsx
@@ -6,6 +6,7 @@ import Sidebar from '../Sidebar/Sidebar';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import logo from '../../images/navlogo.png';
 import photo from './../../images/original_artwork.jpg';
+import { Link } from 'react-router-dom';
 
 const CaseStudy = () => {
   const userContext = useContext(UserContext);
@@ -47,43 +48,44 @@ const CaseStudy = () => {
       <div className="container">
         <table>
           <tr>
-            <td>
+            <td className="column-left">
               <div className="card">
                 <div className="card-inner">
-                  <table>
+                  <table className="filter-container">
                     <tr>
                       <td>
-                        <input className="radio-button" type="radio" value="patient-story"></input>Patient Story
+                        <input className="radio-button" name="filter" type="radio" value="patient-story"></input>Patient Story
                       </td>
                     </tr>
                     <tr>
                       <td>
-                        <input className="radio-button" type="radio" value="staff-recognition"></input>
+                        <input className="radio-button" name="filter" type="radio" value="staff-recognition"></input>
                         Staff Recognition
                       </td>
                     </tr>
                     <tr>
                       <td>
-                        <input className="radio-button" type="radio" value="trailing-session"></input>
+                        <input className="radio-button" name="filter" type="radio" value="trailing-session"></input>
                         Trailing Session
                       </td>
                     </tr>
                     <tr>
                       <td>
-                        <input className="radio-button" type="radio" value="equipment-received"></input>
+                        <input className="radio-button" name="filter" type="radio" value="equipment-received"></input>
                         Equipment Received
                       </td>
                     </tr>
                     <tr>
                       <td>
-                        <input className="radio-button" type="radio" value="other"></input>
+                        <input className="radio-button" name="filter" type="radio" value="other"></input>
                         Other
                       </td>
                     </tr>
                   </table>
                 </div>
+                
               </div>
-              <button className="button">+ Add Case Study</button>
+              <Link to='/case-studies/new'><button className="button-new-case">+ Add Case Study</button></Link>
             </td>
             <td className="column-right">
               <input className="input" type="text" placeholder="Search Case Studies..."></input>
@@ -103,7 +105,8 @@ const CaseStudy = () => {
                           <p>{caseStudies.response}</p>
                         </td>
                         <td className="case-study-block-button">
-                          <button className="button">View</button>
+                          <Link to={`/case-studies/${caseStudies.id}`}><button className="button">View</button></Link>
+                          
                         </td>
                       </tr>
                     </table>
