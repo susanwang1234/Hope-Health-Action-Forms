cfdd3c090e3120b0e9f78190886ad432b18e4a05
Alden Ilao <ailao@sfu.ca>
2021-11-15 22:10:26 -0800

    97: included optional parameter for axios configuration

--- ceres-client/src/services/httpService.ts
+++ ceres-client/src/services/httpService.ts
@@ -3,20 +3,20 @@ axios.defaults.withCredentials = true;
 
 const baseApiUrl = process.env.REACT_APP_DEPLOYMENT_API_URL || 'http://localhost:8080';
 
-const get = (url: string): Promise<any> => {
-  return axios.get(baseApiUrl + url);
+const get = (url: string, config: object = {}): Promise<any> => {
+  return axios.get(baseApiUrl + url, config);
 };
 
-const post = (url: string, obj: any): Promise<any> => {
-  return axios.post(baseApiUrl + url, obj);
+const post = (url: string, obj: any, config: object = {}): Promise<any> => {
+  return axios.post(baseApiUrl + url, obj, config);
 };
 
-const put = (url: string, obj: any): Promise<any> => {
-  return axios.put(baseApiUrl + url, obj);
+const put = (url: string, obj: any, config: object = {}): Promise<any> => {
+  return axios.put(baseApiUrl + url, obj, config);
 };
 
-const patch = (url: string, obj: any): Promise<any> => {
-  return axios.patch(baseApiUrl + url, obj);
+const patch = (url: string, obj: any, config: object = {}): Promise<any> => {
+  return axios.patch(baseApiUrl + url, obj, config);
 };
 
 export default {
