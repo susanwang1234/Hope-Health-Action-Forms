4c20c8944a90942d21b8a240b72d900dc3076939
rgoyal <ritika_goyal@sfu.ca>
2021-11-17 17:42:18 -0800

    93: added validation check for image and checkbox before submit. Added toast messages

--- ceres-client/src/components/Login/Login.tsx
+++ ceres-client/src/components/Login/Login.tsx
@@ -6,6 +6,7 @@ import { useHistory } from 'react-router-dom';
 import { useContext, useState } from 'react';
 import { UserContext } from '../../UserContext';
 import AuthService from '../../services/authService';
+import { toast } from 'react-toastify';
 
 interface FormData {
   username: string;
@@ -32,8 +33,8 @@ function Login() {
       history.push('/departments');
     } else {
       const { msg } = data;
-      window.alert('Invalid Username or Password');
       // do something with error message
+      toast.error('Invalid Username or Password', {position: "top-center"})
     }
   };
 
