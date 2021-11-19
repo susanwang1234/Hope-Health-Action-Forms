c3950e573d82a795fb1b61594ef2a1f4d9027da5
Keegan McGinnis <kmcginni@sfu.ca>
2021-11-18 22:33:16 -0800

    108: Switch to using sync parser

--- ceres-server/src/db/types/DataExporter.ts
+++ ceres-server/src/db/types/DataExporter.ts
@@ -1,5 +1,4 @@
-const { createReadStream, createWriteStream } = require('fs');
-const { Transform, AsyncParser } = require('json2csv');
+const { Parser } = require('json2csv');
 const fs = require('fs');
 import { Response } from 'express';
 
@@ -20,33 +19,17 @@ export class DataExporter {
   async getFileToSendToUser(res: Response) {
     const fields = ['field1', 'field2', 'field3'];
     const opts = { fields };
-    const transformOpts = { highWaterMark: 16384, encoding: 'utf-8' };
-    const inputObj = JSON.stringify({
+    const inputObj = {
       field1: 1,
       field2: 2,
       field3: 3
-    });
-    const outputPath = 'test.csv';
-    // const output = createWriteStream(outputPath, { encoding: 'utf8' });
+    };
 
-    const asyncParser = new AsyncParser(opts, transformOpts);
-    let csv = '';
-    asyncParser.processor
-      .on('data', (chunk: any) => (csv += chunk.toString()))
-      .on('end', () =>
-        // fs.writeFile(outputPath, csv, 'utf8', (err: any) => {
-        //   console.log('IN CALLBACK');
-        // })
-        res.send({ file: csv })
-      )
-      .on('error', (error: any) => console.error(error));
-
-    asyncParser.input.push(inputObj);
-    asyncParser.input.push(null);
-    // const input = createReadStream(inputObj, { encoding: 'utf8' });
-    // const json2csv = new Transform(opts, transformOpts);
-    // console.log('json2csv:', json2csv);
-    // // const processor = input.pipe(json2csv).pipe(output);
-    // json2csv(inputObj).pipe(output);
+    const json2csv = new Parser(opts);
+    const csv = json2csv.parse(inputObj);
+    console.log('CSV:', csv);
+    res.header('Content-Type', 'text/csv');
+    res.attachment('test.csv');
+    return res.send(csv);
   }
 }
