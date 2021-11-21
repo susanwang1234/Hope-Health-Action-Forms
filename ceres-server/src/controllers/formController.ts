6af29a789cfb3821870326db87096e262b497a1b
Keegan McGinnis <kmcginni@sfu.ca>
2021-11-21 15:55:35 -0800

    108: Add check to make sure form exists before fetching responses

--- ceres-server/src/controllers/formController.ts
+++ ceres-server/src/controllers/formController.ts
@@ -46,26 +46,36 @@ const exportFormAsCsv = async (req: Request, res: Response, next: NextFunction) 
     res.status(400).send(formNegativeOrNanInputError);
     return;
   }
-  const form = await Knex.select('Form.id', Knex.raw('month(createdAt) AS month'), Knex.raw('year(createdAt) as year'), 'Department.name')
-    .from('Form')
-    .join('Department', 'Form.departmentId', '=', 'Department.id')
-    .where('Form.id', formId)
-    .first();
 
-  const formResponses = await Knex.select('*')
-    .from('FormResponse')
-    .join('DepartmentQuestion', 'FormResponse.departmentQuestionId', '=', 'DepartmentQuestion.id')
-    .join('Question', 'DepartmentQuestion.questionId', '=', 'Question.id')
-    .where('FormResponse.formId', formId);
+  try {
+    const form = await Knex.select('Form.id', Knex.raw('month(createdAt) AS month'), Knex.raw('year(createdAt) as year'), 'Department.name')
+      .from('Form')
+      .join('Department', 'Form.departmentId', '=', 'Department.id')
+      .where('Form.id', formId)
+      .first();
 
-  const filename = `${form.id}${form.name}-report-${form.month}-${form.year}.csv`;
-  res.header('Content-Type', 'text/csv');
-  res.attachment(filename);
+    if (!form) {
+      res.status(400).send({ message: 'Form does not exist' });
+      return;
+    }
 
-  const fileExportFormatPolicy: FileExportFormatPolicy = new CsvFormatPolicy();
-  const dataExporter: DataFormatter = new DataFormatter(formResponses, fileExportFormatPolicy);
-  const file = dataExporter.getFileToSendToUser();
-  res.send(file);
+    const formResponses = await Knex.select('*')
+      .from('FormResponse')
+      .join('DepartmentQuestion', 'FormResponse.departmentQuestionId', '=', 'DepartmentQuestion.id')
+      .join('Question', 'DepartmentQuestion.questionId', '=', 'Question.id')
+      .where('FormResponse.formId', formId);
+
+    const filename = `${form.id}${form.name}-report-${form.month}-${form.year}.csv`;
+    res.header('Content-Type', 'text/csv');
+    res.attachment(filename);
+
+    const fileExportFormatPolicy: FileExportFormatPolicy = new CsvFormatPolicy();
+    const dataExporter: DataFormatter = new DataFormatter(formResponses, fileExportFormatPolicy);
+    const file = dataExporter.getFileToSendToUser();
+    res.send(file);
+  } catch (error: any) {
+    res.status(500).send(error);
+  }
 };
 
 export default { createNewForm, getAllFormsByDepartmentId, exportFormAsCsv };
