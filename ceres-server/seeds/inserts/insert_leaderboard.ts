14742a7182a1670327188da096f177c959f39e50
jyw30 <susan_wang_3@sfu.ca>
2021-11-20 22:48:34 -0800

    90: removed leaderboard file

--- ceres-server/seeds/inserts/insert_leaderboard.ts
+++
@@ -1,8 +0,0 @@
-import { Knex } from 'knex';
-
-export async function insertLeaderboard(knex: Knex): Promise<void> {
-  await knex('Leaderboard').insert([
-    { id: 1, name: 'Department' },
-    { id: 2, name: 'Score' }
-  ]);
-}
