b832119429b12986b7579b2d652406eeb2b5bf81
Alireza Ahmadi <aaa120@sfu.ca>
2021-11-24 20:29:28 -0800

    updates messageBoard schema and seeds to include department ID in order to display the messages based on user departments

--- ceres-server/seeds/inserts/insert_messages.ts
+++ ceres-server/seeds/inserts/insert_messages.ts
@@ -6,9 +6,15 @@ export async function insertMessages(knex: Knex): Promise<void> {
       id: 1,
       author: 'admin',
       message_content:
-        'Good evening everyone, this is a friendly reminder that tommorow he have our monthly meeting at 6:00 pm. try to be on time before all snacks are gone. looking forward too seeing you all!'
+        'Good evening everyone, this is a friendly reminder that tommorow he have our monthly meeting at 6:00 pm. try to be on time before all snacks are gone. looking forward too seeing you all!',
+      departmentName: 'Rehab'
     },
-    { id: 2, author: 'admin', message_content: 'We support and stand by @HaitiCancerCenter in their fight aigains cancer! #go_away_cancer' },
-    { id: 3, author: 'admin', message_content: 'dfud fiash aisu wr0uas gfgia 0ehda ahfasa gvjxc w0sdvha 0f sasdh afasug as sia dfg awru vlzn cvpzxi bsfpihg  pdvbjfs sfbjpzs bshfsdi dagpibh aehtey' }
+    { id: 2, author: 'admin', message_content: 'We support and stand by @HaitiCancerCenter in their fight aigains cancer! #go_away_cancer', departmentName: 'Rehab' },
+    {
+      id: 3,
+      author: 'admin',
+      message_content: 'dfud fiash aisu wr0uas gfgia 0ehda ahfasa gvjxc w0sdvha 0f sasdh afasug as sia dfg awru vlzn cvpzxi bsfpihg  pdvbjfs sfbjpzs bshfsdi dagpibh aehtey',
+      departmentName: 'NICUPaeds'
+    }
   ]);
 }
