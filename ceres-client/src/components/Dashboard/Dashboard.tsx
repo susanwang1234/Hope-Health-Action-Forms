e719470e4beec26a682b5abbb7ea4d141ac65719
jyw30 <susan_wang_3@sfu.ca>
2021-11-25 22:43:54 -0800

    90: refactored to do list

--- ceres-client/src/components/Dashboard/Dashboard.tsx
+++ ceres-client/src/components/Dashboard/Dashboard.tsx
@@ -1,17 +1,14 @@
 import '../../App.css';
 import './Dashboard.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import logo from '../../images/navlogo.png';
-import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import { IoIosAlert } from 'react-icons/io';
-import { IoIosCheckmarkCircle } from 'react-icons/io';
 import { IoIosInformationCircle } from 'react-icons/io';
 import Leaderboard from './Leaderboard';
 import EmployeeOfTheMonth from './EmployeeOfTheMonth';
-
+import ToDo from './ToDo';
 
 /* Citations: 
     https://github.com/mustafaerden/react-admin-dashboard
@@ -20,25 +17,12 @@ import EmployeeOfTheMonth from './EmployeeOfTheMonth';
 
 const Dashboard = () => {
   const [showNav, setShowNav] = useState(false);
-  const [date, setDate]: any = useState(new Date());
-  
+
   const instructions = (event: any) => {
     alert(
       'Here is how you get points:\n\n Each department will receive a point for completing and submitting their MSPP data for the month on time. \n\n Each department will receive a point everytime they submit a new case study. \n\n The Employee of the Month will receive 3 points for the department they reside in.'
     );
   };
-
-  
-
-  function generateCalendar() {
-    return (
-      <div className="app">
-        <div className="calendar-container">
-          <Calendar onChange={setDate} value={date} selectRange={true} className="responsive-calendar flex-shrink" />
-        </div>
-      </div>
-    );
-  }
 
   return (
     <html>
@@ -56,32 +40,18 @@ const Dashboard = () => {
             <div className="dashboard-cards">
               <div className="card-outer fill-space-left">
                 <p className="title">To Do</p>
-                <div className="card-inner width-100-percent">
-                  <div className="align-left">
-                    <div className="due-content">
-                      <IoIosCheckmarkCircle className="icon icon-case-study" /> Case Study <br />
-                      Due October 31 2021
-                    </div>
-                    <div className="due-content">
-                      <IoIosAlert className="icon icon-mspp-report" /> MSPP Report <br />
-                      <div className="due-in-red">Due October 31 2021</div> <br />
-                    </div>
-                  </div>
-                  <div className="align-right flex">{generateCalendar()}</div>
-                </div>
+                <div className="card-inner width-100-percent">{ToDo()}</div>
 
-                <p className="title">Leaderboard
-                <IoIosInformationCircle className="align-right icon instructions" onClick={(instructionButtonEvent) => instructions(instructionButtonEvent)} /></p>
-                <div className="card-inner width-100-percent">
-                  {Leaderboard()}
-                </div>
+                <p className="title">
+                  Leaderboard
+                  <IoIosInformationCircle className="align-right icon instructions" onClick={(instructionButtonEvent) => instructions(instructionButtonEvent)} />
+                </p>
+                <div className="card-inner width-100-percent">{Leaderboard()}</div>
               </div>
 
               <div className="card-outer fill-space-right">
                 <p className="title">Employee of the Month</p>
-                <div className="card-inner height-100-percent">
-                 {EmployeeOfTheMonth()}
-                </div>
+                <div className="card-inner height-100-percent">{EmployeeOfTheMonth()}</div>
               </div>
             </div>
           </div>
---
+++ ceres-client/src/components/Dashboard/ToDo.tsx
@@ -0,0 +1,40 @@
+import './Dashboard.css';
+import Calendar from "react-calendar";
+import { useState } from 'react';
+import { IoIosAlert } from 'react-icons/io';
+import { IoIosCheckmarkCircle } from 'react-icons/io';
+
+const ToDo = () => {
+
+    const [date, setDate]: any = useState(new Date());
+
+    function generateCalendar() {
+        return (
+          <div className="app">
+            <div className="calendar-container">
+              <Calendar onChange={setDate} value={date} selectRange={true} className="responsive-calendar flex-shrink" />
+            </div>
+          </div>
+        );
+      }
+
+
+      return(
+          <>
+        <div className="align-left">
+        <div className="due-content">
+          <IoIosCheckmarkCircle className="icon icon-case-study" /> Case Study <br />
+          Due Oct 31 2021
+        </div>
+        <div className="due-content">
+          <IoIosAlert className="icon icon-mspp-report" /> Monthly Data Report <br />
+          <div className="due-in-red">Due Dec 25 2021</div> <br />
+        </div>
+      </div>
+      <div className="align-right flex">{generateCalendar()}</div>
+      </>
+      )
+
+
+}
+export default ToDo;
