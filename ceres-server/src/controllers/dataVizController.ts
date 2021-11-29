49ba289e2aa7fa753451de40f5c941194080f1fe
Keegan McGinnis <kmcginni@sfu.ca>
2021-11-28 17:27:25 -0800

    123: Add query for getting raw data for dataviz

--- ceres-server/src/controllers/dataVizController.ts
+++ ceres-server/src/controllers/dataVizController.ts
@@ -5,7 +5,21 @@ import { Knex } from '../db/mysql';
 const NAMESPACE = 'Data Visualization Control';
 
 const getDataForPlots = async (req: Request, res: Response, next: NextFunction) => {
-  res.send({ message: 'Request received' });
+  const rawData = await Knex.select('FormResponse.response', 'createdAt', 'label', 'departmentQuestionId')
+    .from('FormResponse')
+    .join('DepartmentQuestion', 'FormResponse.departmentQuestionId', 'DepartmentQuestion.id')
+    .join('Question', 'DepartmentQuestion.questionId', 'Question.id')
+    .join('Form', 'FormResponse.formId', 'Form.id')
+    .orderBy('departmentQuestionId', 'createdAt');
+
+  // const data = {};
+  // for (const response of rawData) {
+  //   const departmentQuestionId: number = response.departmentQuestionId
+  //   if (data.hasOwnProperty(departmentQuestionId)) {
+  //     data.
+  //   }
+  // }
+  res.send(rawData);
 };
 
 export default { getDataForPlots };
