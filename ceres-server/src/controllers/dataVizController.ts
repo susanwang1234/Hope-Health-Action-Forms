a436c3ff3d1cc61f7f8aa759bf13005d313d08af
Keegan McGinnis <kmcginni@sfu.ca>
2021-11-29 01:32:13 -0800

    123: Add filtering to backend for graphs

--- ceres-server/src/controllers/dataVizController.ts
+++ ceres-server/src/controllers/dataVizController.ts
@@ -5,13 +5,28 @@ import { Knex } from '../db/mysql';
 const NAMESPACE = 'Data Visualization Control';
 
 const getDataForPlots = async (req: Request, res: Response, next: NextFunction) => {
-  const rawData = await Knex.select('FormResponse.response', 'createdAt', 'label', 'departmentQuestionId')
+  const query = Knex.select('FormResponse.response', 'createdAt', 'label', 'departmentQuestionId')
     .from('FormResponse')
     .join('DepartmentQuestion', 'FormResponse.departmentQuestionId', 'DepartmentQuestion.id')
     .join('Question', 'DepartmentQuestion.questionId', 'Question.id')
     .join('Form', 'FormResponse.formId', 'Form.id')
     .orderBy('departmentQuestionId', 'createdAt');
 
+  const startMonth = req.query.startMonth;
+  const startYear = req.query.startYear;
+  if (startMonth && startYear) {
+    const startDate = new Date(`${startMonth} ${startYear}`);
+    query.where('createdAt', '>=', startDate);
+  }
+
+  const endMonth = req.query.endMonth;
+  const endYear = req.query.endYear;
+  if (endMonth && endYear) {
+    const endDate = new Date(`${endMonth} ${endYear}`);
+    query.where('createdAt', '<=', endDate);
+  }
+
+  const rawData = await query;
   const data: any = {
     plotData: [],
     questionLabels: []
