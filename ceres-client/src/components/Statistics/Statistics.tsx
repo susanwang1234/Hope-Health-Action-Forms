33b7ca6bde4f90ce30e7927de7eac44d69d76802
Keegan McGinnis <kmcginni@sfu.ca>
2021-11-30 01:08:36 -0800

    124: Start using flexbox for statistics page

--- ceres-client/src/components/Statistics/Statistics.tsx
+++ ceres-client/src/components/Statistics/Statistics.tsx
@@ -10,6 +10,7 @@ import Plot from 'react-plotly.js';
 const MONTHS = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
 
 const StatisticsDashboard = () => {
+  document.body.style.backgroundColor = '#f5f5f5';
   const [showNav, setShowNav] = useState(false);
   const [dataForPlots, setDataForPlots] = useState<any>([]);
   const [questionLabels, setQuestionLabels] = useState([]);
@@ -34,6 +35,14 @@ const StatisticsDashboard = () => {
     }
   }
 
+  // function resetDateFilter() {
+  //   setStartMonth('');
+  //   setStartYear(0);
+  //   setEndMonth('');
+  //   setEndYear(0);
+  //   // fetchData();
+  // }
+
   const radioButtonHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     setPlotIndex(+value);
@@ -46,11 +55,38 @@ const StatisticsDashboard = () => {
         <img src={logo} alt="Logo" className="logo" />
       </header>
       <Sidebar show={showNav}></Sidebar>
-      <div className="grid-container">
-        <div className="dashboard-title">
+      <div className="outer-container">
+      <div className="dashboard-title">
           Rehab Statistics
         </div>
-        <div className="date-filter">
+      <div className="statistics-dashboard-container">
+        <div className="left-container">
+          {/* <div className="card question-list"> */}
+            <ul className="questionMenu">
+              {questionLabels.map((label, index) => <li><input className="radio-button" name="selected" type="radio" value={index} onChange={radioButtonHandler}></input>{label}</li>)}
+            </ul>
+          {/* </div> */}
+        </div>
+        {/* <div className="right-container"> */}
+          {/* <div className="plot"> */}
+            {dataForPlots[0] &&
+            <Plot
+              className="right-container"
+              data={[
+                {
+                  x: dataForPlots[plotIndex].x,
+                  y: dataForPlots[plotIndex].y,
+                  type: 'scatter',
+                  mode: 'lines+markers',
+                },
+
+              ]}
+              layout={ {title: questionLabels[plotIndex] } }
+            />}
+          {/* </div> */}
+        {/* </div> */}
+      </div>
+        {/* <div className="date-filter">
           From
           <select defaultValue="" onChange={event => setStartMonth(event.target.value)}>
             <option className="placeholder" value="" disabled>Start Month</option>
@@ -62,15 +98,16 @@ const StatisticsDashboard = () => {
             <option className="placeholder" value="" disabled>End Month</option>
             {MONTHS.map(month => <option>{month}</option>)}
           </select>
-          <input onChange={event => setEndYear(+event.target.value)} type="number" min="1970" max="3000" placeholder="End Year"></input>
+          <input id="startYear" value={startYear} onChange={event => setEndYear(+event.target.value)} type="number" min="1970" max="3000" placeholder="End Year"></input>
           <button onClick={fetchData}>Search</button>
-        </div>
-        <div className="card question-list">
+          <button>Reset</button>
+        </div> */}
+        {/* <div className="card question-list">
           <ul className="questionMenu">
             {questionLabels.map((label, index) => <li><input className="radio-button" name="selected" type="radio" value={index} onChange={radioButtonHandler}></input>{label}</li>)}
           </ul>
-        </div>
-        <div className="plot">
+        </div> */}
+        {/* <div className="plot">
           {dataForPlots[0] &&
           <Plot
             data={[
@@ -83,10 +120,11 @@ const StatisticsDashboard = () => {
 
             ]}
             layout={ {title: questionLabels[plotIndex] } }
-          />}
+          />} */}
+        {/* </div> */}
         </div>
       </div>
-    </div>
+    // {/* </div> */}
   )
 }
 
