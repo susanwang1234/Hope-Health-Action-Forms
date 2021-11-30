ee2a1675bcc3ab3c7ca8e786a133009e073ca718
Alden Ilao <ailao@sfu.ca>
2021-11-30 15:36:21 -0800

    edited user password is hashed

--- ceres-server/src/controllers/userController.ts
+++ ceres-server/src/controllers/userController.ts
@@ -8,6 +8,7 @@ import { userNegativeOrNanInputError, userDNEError } from 'shared/errorMessages'
 import authUtil from '../utils/authHelper';
 import userModel from '../db/models/userModel';
 import logging from '../config/logging';
+import { isInvalidInput } from './controllerTools/isInvalidInput';
 
 const NAMESPACE = 'User Control';
 const TABLE_NAME = 'User';
@@ -15,6 +16,11 @@ const TABLE_NAME = 'User';
 const inputtedReqBody = (req: Request) => {
   const { username, password, departmentId, roleId } = req.body;
   return { username: username, password: password, departmentId: departmentId, roleId: roleId };
+};
+
+const isUsernameUnique = async (username: string) => {
+  const userFound = await userModel.findOne('User.username', username);
+  return userFound;
 };
 
 const getUsers = async (req: Request, res: Response, next: NextFunction) => {
@@ -39,7 +45,31 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 const editUserById = async (req: Request, res: Response, next: NextFunction) => {
-  await editItemById(req, res, next, NAMESPACE, TABLE_NAME, userNegativeOrNanInputError, userDNEError, inputtedReqBody(req));
+  try {
+    const editedUser = inputtedReqBody(req);
+    const userFound = await userModel.findOne('User.username', editedUser.username);
+    let isSameUser: boolean = true;
+
+    const userId: number = +req.params.id;
+    if (isInvalidInput(userId)) {
+      res.status(400).send(userNegativeOrNanInputError);
+      return;
+    }
+
+    if (userFound) {
+      isSameUser = userId === userFound.id;
+    }
+
+    if (!isSameUser) {
+      res.status(400).send({ error: 'Username already in use by other account' });
+      return;
+    }
+    editedUser.password = await authUtil.hashPassword(editedUser.password);
+    await editItemById(req, res, next, NAMESPACE, TABLE_NAME, userNegativeOrNanInputError, userDNEError, editedUser);
+  } catch (error: any) {
+    logging.error(NAMESPACE, error.message, error);
+    res.status(500).send(error);
+  }
 };
 
 const deleteUserById = async (req: Request, res: Response, next: NextFunction) => {
