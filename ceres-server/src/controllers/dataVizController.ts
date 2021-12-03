dc302b65129bd6567bc2fe751a7d96fa89704a2c
Keegan McGinnis <kmcginni@sfu.ca>
2021-12-02 19:45:20 -0800

    124: Add check for negative or not a number department id in dataviz controller

--- ceres-server/src/controllers/dataVizController.ts
+++ ceres-server/src/controllers/dataVizController.ts
@@ -1,16 +1,23 @@
 import logging from '../config/logging';
 import { Request, Response, NextFunction } from 'express';
 import { Knex } from '../db/mysql';
+import { isInvalidInput } from './controllerTools/isInvalidInput';
 
 const NAMESPACE = 'Data Visualization Control';
 
 const getDataForPlots = async (req: Request, res: Response, next: NextFunction) => {
+  const departmentId = +req.params.departmentId;
+  if (isInvalidInput(departmentId)) {
+    res.status(400).send({ error: 'Incorrect usage for /dataviz/:departmentId, departmentId must be a positive integer' });
+    return;
+  }
   const query = Knex.select('FormResponse.response', 'createdAt', 'label', 'departmentQuestionId')
     .from('FormResponse')
     .join('DepartmentQuestion', 'FormResponse.departmentQuestionId', 'DepartmentQuestion.id')
     .join('Question', 'DepartmentQuestion.questionId', 'Question.id')
     .join('Form', 'FormResponse.formId', 'Form.id')
-    .orderBy('departmentQuestionId', 'createdAt');
+    .orderBy('departmentQuestionId', 'createdAt')
+    .where('Form.departmentId', departmentId);
 
   const startMonth = req.query.startMonth;
   const startYear = req.query.startYear;
