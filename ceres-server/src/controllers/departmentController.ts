cd27cf33ebbbaf562b3c9848e6f05fec2351a8e2
Keegan McGinnis <kmcginni@sfu.ca>
2021-12-03 22:51:51 -0800

    124: Add backend service for getting single department

--- ceres-server/src/controllers/departmentController.ts
+++ ceres-server/src/controllers/departmentController.ts
@@ -1,6 +1,7 @@
 import logging from '../config/logging';
 import { Request, Response, NextFunction } from 'express';
 import { getItems } from './requestTemplates/getAllRequest';
+import { Knex } from 'db/mysql';
 
 const NAMESPACE = 'Department Control';
 const TABLE_NAME = 'Department';
@@ -9,4 +10,22 @@ const getDepartments = async (req: Request, res: Response, next: NextFunction) =
   await getItems(req, res, next, NAMESPACE, TABLE_NAME);
 };
 
-export default { getDepartments };
+const getDepartmentById = async (req: Request, res: Response, next: NextFunction) => {
+  logging.info(NAMESPACE, 'FETCHING DEPARTMENT BY ID');
+  const departmentId = +req.params.departmentId;
+  if (!departmentId) {
+    res.status(404).send({ error: 'Department id must be positive integer' });
+    return;
+  }
+
+  try {
+    const department = await Knex.select('*').from('Department').where('id', departmentId).first();
+    logging.info('NAMESPACE', `RETRIEVED DEPARTMENT WITH ID ${departmentId}`);
+    res.send(department);
+  } catch (error: any) {
+    logging.error(NAMESPACE, error.message, error);
+    res.status(500).send(error);
+  }
+};
+
+export default { getDepartments, getDepartmentById };
